b238dd718f3ec657d940b76b52c929cf3175107f
Loom <looom@gmx.de>
2019-12-27 12:56:35 +0100

    Issue #2: use Units directly in ElementImpl, added Unit implementation

--- src/main/kotlin/de/gleex/pltcmd/model/elements/ElementImpl.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/elements/ElementImpl.kt
@@ -3,12 +3,12 @@ package de.gleex.pltcmd.model.elements
 /**
  * Implements an [Element] with a set of [Unit]s and subordinate [Element]s to whom orders are relayed.
  */
-data class ElementImpl(val members: Set<UnitElement>, val subordinates: Set<Element>) : Element {
+data class ElementImpl(val members: Set<Unit>, val subordinates: Set<Element>) : Element {
 
 	override fun executeOrder() {
-		// let members execute orders
+		// TODO let members execute orders
 		members.forEach {
-			it.executeOrder()
+			//it.executeOrder()
 		}
 		// relay order to all subordinates
 		subordinates.forEach {
