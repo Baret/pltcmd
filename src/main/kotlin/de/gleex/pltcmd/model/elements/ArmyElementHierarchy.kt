a58ff9261891ca1c8ca9618f91050f1b68abf011
Loom <looom@gmx.de>
2019-12-27 15:15:05 +0100

    Issue #2: Element factory in ArmyElementHierarchy + test

--- src/main/kotlin/de/gleex/pltcmd/model/elements/ArmyElementHierarchy.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/elements/ArmyElementHierarchy.kt
@@ -3,7 +3,7 @@ package de.gleex.pltcmd.model.elements
 /**
  * @see [Army Hierarchy](https://en.wikipedia.org/wiki/Military_unit#Army_hierarchy)
  */
-enum class ArmyElementHierarchy(val minCountOfConstituentUnits : Int, val maxCountOfConstituentUnits : Int) {
+enum class ArmyElementHierarchy(val minCountOfConstituentUnits: Int, val maxCountOfConstituentUnits: Int) {
 	/** Formation, strength: 6,000 to 20,000  */
 	Division(2, 4),
 	/** Formation, strength: 3,000 to 5,000  */
@@ -19,6 +19,32 @@ enum class ArmyElementHierarchy(val minCountOfConstituentUnits : Int, val maxCou
 	/** strength: 2 to 4  */
 	Fireteam(1, 2),
 	/** strength: 2  */
-	BuddyTeam(0, 0)
-	
+	BuddyTeam(0, 0) {
+		override fun createElement(): Element {
+			val soldier1 = GenericUnit(UnitType.Soldier)
+			val soldier2 = GenericUnit(UnitType.Soldier)
+			return ElementImpl(setOf(soldier1, soldier2), setOf())
+		}
+	};
+
+	fun averageCountOfConsituentUnits(): Int {
+		return (minCountOfConstituentUnits + maxCountOfConstituentUnits) / 2
+	}
+
+	/** Creates a default Element with the average number of constituent units for every subordinate element. */
+	open fun createElement(): Element {
+		val hierarchy = values()
+		val myIndex = hierarchy.indexOf(this)
+		val subordinates = mutableSetOf<Element>()
+		if (myIndex + 1 < hierarchy.size) {
+			val averageCount = averageCountOfConsituentUnits()
+			val consituentUnitType = hierarchy[myIndex + 1]
+			for (i in 0 until averageCount) {
+				val subordinate = consituentUnitType.createElement()
+				subordinates.add(subordinate)
+			}
+		}
+		return ElementImpl(setOf(), subordinates)
+	}
+
 }
---
+++ src/test/kotlin/de/gleex/pltcmd/model/elements/ArmyElementHierarchyTest.kt
@@ -0,0 +1,58 @@
+package de.gleex.pltcmd.model.elements
+
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+import org.junit.jupiter.api.Test
+
+class ArmyElementHierarchyTest {
+
+	@Test
+	fun createElement() {
+		val division = ArmyElementHierarchy.Division.createElement()
+
+		assertHierarchy(
+			division,
+			3/*brigades*/,
+			4/*battalions*/,
+			4/*companies*/,
+			5/*platoons*/,
+			4/*squads*/,
+			2/*fireteams*/,
+			1/*buddy team*/
+		)
+	}
+
+	private fun assertHierarchy(actual: Element, vararg expectedCounts: Int) {
+		assertNotNull(actual)
+		assertTrue(actual is ElementImpl)
+
+		val actualSubordinates = actual.subordinates
+		val actualMembers = actual.members
+		if (actualSubordinates.isEmpty()) {
+			assertBuddyTeam(actualMembers)
+		} else {
+			assertEquals(setOf(), actualMembers)
+			assertSubHierarchy(actual, *expectedCounts)
+		}
+	}
+
+	private fun assertBuddyTeam(actualMembers: Set<Unit>) {
+		assertEquals(2, actualMembers.size)
+		actualMembers.forEach() {
+			assertTrue(it.isOfType(UnitType.Soldier))
+		}
+
+	}
+
+	private fun assertSubHierarchy(actual: ElementImpl, vararg expectedCounts: Int) {
+		val expectedSubordinateCounts = expectedCounts.firstOrNull() ?: 0;
+		val remainingCounts = expectedCounts.drop(1)
+		val actualSubordinates = actual.subordinates
+		assertEquals(expectedSubordinateCounts, actualSubordinates.size)
+		actualSubordinates.forEach {
+			assertHierarchy(it, *remainingCounts.toIntArray())
+		}
+	}
+
+}
