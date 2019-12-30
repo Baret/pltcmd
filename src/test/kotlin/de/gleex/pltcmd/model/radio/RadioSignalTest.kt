ccd8e1448bd33e3da29f5a740bf7fa550b6d3bbf
Baret <Beret14@web.de>
2019-12-30 22:11:50 +0100

    testing build fail with failing unittest

--- src/test/kotlin/de/gleex/pltcmd/model/radio/RadioSignalTest.kt
+++ src/test/kotlin/de/gleex/pltcmd/model/radio/RadioSignalTest.kt
@@ -41,7 +41,7 @@ class RadioSignalTest {
                 .98,
                 rs.along(listOf(Terrain(TerrainType.GRASSLAND, TerrainHeight.ONE))))
         assertEquals(
-                .98.pow(3),
+                .97.pow(3),
                 rs.along(listOf(Terrain(TerrainType.GRASSLAND, TerrainHeight.ONE),
                                 Terrain(TerrainType.GRASSLAND, TerrainHeight.ONE),
                                 Terrain(TerrainType.GRASSLAND, TerrainHeight.ONE))))
