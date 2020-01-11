1a6d151c1680e7dd65843cd127c04449af5fb964
Loom <looom@gmx.de>
2020-01-11 11:58:24 +0100

    Each location of the world is unique so the WorlTiles must alse be unique by their Coordinates.

--- src/main/kotlin/de/gleex/pltcmd/model/world/WorldTile.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/world/WorldTile.kt
@@ -15,4 +15,17 @@ data class WorldTile(val coordinate: Coordinate, val terrain: Terrain) {
 		Terrain(TerrainType.GRASSLAND, TerrainHeight.THREE)
 	)
 
+	/** Only a single tile per coordinate, as the place on the map is unique */
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (other?.javaClass != javaClass) return false
+		other as WorldTile
+		return coordinate.equals(other.coordinate)
+	}
+
+	/** Only a single tile per coordinate, as the place on the map is unique */
+	override fun hashCode(): Int {
+		return coordinate.hashCode()
+	}
+
 }
