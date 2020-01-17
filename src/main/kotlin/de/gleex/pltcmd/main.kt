eb183d871c60cb02bdd33ecb757e26fef6c8005f
Baret <beret14@web.de>
2020-01-17 22:26:57 +0100

    moved the viewable map to a more realistic sector

--- src/main/kotlin/de/gleex/pltcmd/main.kt
+++ src/main/kotlin/de/gleex/pltcmd/main.kt
@@ -7,15 +7,17 @@ import de.gleex.pltcmd.model.world.WorldMap
 import de.gleex.pltcmd.options.UiOptions
 import de.gleex.pltcmd.ui.GameView
 import org.hexworks.zircon.api.SwingApplications
+import org.hexworks.zircon.api.data.impl.Position3D
 
 
 fun main() {
     val worldMap = WorldMap(
             setOf(
                     Sector.generateAt(
-                            Coordinate(0, 0))))
+                            Coordinate(350, 200))))
 
     val application = SwingApplications.startApplication(UiOptions.buildAppConfig())
     val gameWorld = GameWorld(worldMap)
+    gameWorld.scrollTo3DPosition(Position3D.create(350, 200,0))
     application.dock(GameView(gameWorld))
 }
