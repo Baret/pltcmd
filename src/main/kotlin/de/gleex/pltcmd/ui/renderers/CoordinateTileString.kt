fb624df92793b35fcca575971100ac3b41d6d88e
Loom <looom@gmx.de>
2020-01-18 10:56:21 +0100

    moved drawing of coordinate text to own class

---
+++ src/main/kotlin/de/gleex/pltcmd/ui/renderers/CoordinateTileString.kt
@@ -0,0 +1,76 @@
+package de.gleex.pltcmd.ui.renderers
+
+import org.hexworks.zircon.api.data.CharacterTile
+import org.hexworks.zircon.api.data.Position
+import org.hexworks.zircon.api.graphics.CharacterTileString
+import org.hexworks.zircon.api.graphics.DrawSurface
+import org.hexworks.zircon.api.graphics.TileGraphics
+import org.hexworks.zircon.api.resource.TilesetResource
+import org.hexworks.zircon.api.behavior.Drawable
+import org.hexworks.zircon.api.builder.data.TileBuilder
+import org.hexworks.zircon.api.data.Tile
+import de.gleex.pltcmd.game.ColorRepository
+import org.hexworks.zircon.api.Tiles
+import org.hexworks.zircon.api.color.TileColor
+
+/**
+ * Draws a part of a coordinate as text. The major coordinate will be highlighted.
+ * It can be drawn horizontally or vertically.
+ **/
+class CoordinateTileString(coordinateValue: Int, val vertical: Boolean) : Drawable {
+    private val text: String
+    private val majorLength: Int
+    
+    init {
+        text = coordinateValue.toString()
+        val majorCoordinateValue = Math.floorDiv(coordinateValue, 100)
+        majorLength = Math.log10(majorCoordinateValue.toDouble()).toInt()
+    }
+
+    override fun drawOnto(surface: DrawSurface, position: Position) {
+        val builder = Tiles.newBuilder().
+                withBackgroundColor(TileColor.transparent())
+        if (vertical) drawCenteredVertically(surface, position, builder)
+        else drawCenteredHorizontally(surface, position, builder)
+    }
+
+    private fun drawCenteredHorizontally(surface: DrawSurface, pos: Position, builder: TileBuilder) {
+            // center text on position
+            var topTextOffset = offsetForCenteredText(text)
+            // highlight first number
+            builder.withForegroundColor(ColorRepository.COORDINATE_COLOR_HIGHLIGHT_X)
+            for (letter in text) {
+                val top = pos.withRelativeX(topTextOffset)
+                draw(surface, letter, top, builder)
+                topTextOffset++
+                builder.withForegroundColor(ColorRepository.GRID_COLOR)
+            }
+    }
+
+    private fun drawCenteredVertically(surface: DrawSurface, pos: Position, builder: TileBuilder) {
+            // center text on position
+            var leftTextOffset = offsetForCenteredText(text)
+            // highlight first number
+            builder.withForegroundColor(ColorRepository.COORDINATE_COLOR_HIGHLIGHT_Y)
+            for (letter in text) {
+                val left = pos.withRelativeY(leftTextOffset)
+                draw(surface, letter, left, builder)
+                leftTextOffset++
+                builder.withForegroundColor(ColorRepository.GRID_COLOR)
+            }
+    }
+
+    private fun offsetForCenteredText(text: String) = -(text.length / 2)
+
+    private fun draw(surface: DrawSurface, letter: Char, pos: Position, builder: TileBuilder) =
+        surface.draw(builder.withCharacter(letter).buildCharacterTile(), pos)
+
+    private fun createTile(character: Char): Tile {
+        return Tiles.newBuilder().
+                withForegroundColor(ColorRepository.GRID_COLOR).
+                withBackgroundColor(TileColor.transparent()).
+                withCharacter(character).
+                buildCharacterTile()
+    }
+
+}
--- src/main/kotlin/de/gleex/pltcmd/ui/renderers/MapCoordinates.kt
+++ src/main/kotlin/de/gleex/pltcmd/ui/renderers/MapCoordinates.kt
@@ -43,57 +43,14 @@ class MapCoordinates(
 
     private fun drawGridCoordinates(topLeftCoordinate: Coordinate, topLeftPos: Position) {
         // grid coordinates (for square)
-        val builder = Tiles.newBuilder().
-                withBackgroundColor(TileColor.transparent())
         for (i in 1 until size.width step 5) {
             // top
             val topCoord = topLeftCoordinate.withRelativeEasting(i - 1)
-            val topText = topCoord.eastingFromLeft.toString()
-            drawCenteredHorizontally(topText, topLeftPos.withRelativeX(i + 1), builder) // +1 offset by other decoration to map view position
+            draw(CoordinateTileString(topCoord.eastingFromLeft, false), topLeftPos.withRelativeX(i + 1)) // +1 offset by other decoration to map view position
             // left
             val leftCoord = topLeftCoordinate.withRelativeNorthing(-i + 1)
-            val leftText = leftCoord.northingFromBottom.toString()
-            drawCenteredVertically(leftText, topLeftPos.withRelativeY(i + 1), builder) // +1 offset by other decoration to map view position
+            draw(CoordinateTileString(leftCoord.northingFromBottom, true), topLeftPos.withRelativeY(i + 1)) // +1 offset by other decoration to map view position
         }
-    }
-
-    private fun drawCenteredHorizontally(topText: String, pos: Position, builder: TileBuilder) {
-            // center text on position
-            var topTextOffset = offsetForCenteredText(topText)
-            // highlight first number
-            builder.withForegroundColor(ColorRepository.COORDINATE_COLOR_HIGHLIGHT_X)
-            for (letter in topText) {
-                val top = pos.withRelativeX(topTextOffset)
-                draw(letter, top, builder)
-                topTextOffset++
-                builder.withForegroundColor(ColorRepository.GRID_COLOR)
-            }
-    }
-
-    private fun drawCenteredVertically(leftText: String, pos: Position, builder: TileBuilder) {
-            // center text on position
-            var leftTextOffset = offsetForCenteredText(leftText)
-            // highlight first number
-            builder.withForegroundColor(ColorRepository.COORDINATE_COLOR_HIGHLIGHT_Y)
-            for (letter in leftText) {
-                val left = pos.withRelativeY(leftTextOffset)
-                draw(letter, left, builder)
-                leftTextOffset++
-                builder.withForegroundColor(ColorRepository.GRID_COLOR)
-            }
-    }
-
-    private fun offsetForCenteredText(text: String) = -(text.length / 2)
-
-    private fun draw(letter: Char, pos: Position, builder: TileBuilder) =
-        draw(builder.withCharacter(letter).buildCharacterTile(), pos)
-
-    private fun createTile(character: Char): Tile {
-        return Tiles.newBuilder().
-                withForegroundColor(ColorRepository.GRID_COLOR).
-                withBackgroundColor(TileColor.transparent()).
-                withCharacter(character).
-                buildCharacterTile()
     }
 
     override fun toString() = "MapCoordinates $backend"
