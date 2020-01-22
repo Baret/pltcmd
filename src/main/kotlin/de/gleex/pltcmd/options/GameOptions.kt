c7c812bf1923d158f947727263424ec8834bb592
Baret <beret14@web.de>
2020-01-22 21:31:45 +0100

    made attenuationModel a property

--- src/main/kotlin/de/gleex/pltcmd/options/GameOptions.kt
+++ src/main/kotlin/de/gleex/pltcmd/options/GameOptions.kt
@@ -2,18 +2,19 @@ package de.gleex.pltcmd.options
 
 import de.gleex.pltcmd.model.radio.PercentageReducingAttenuation
 import de.gleex.pltcmd.model.world.Sector
+import org.hexworks.cobalt.databinding.api.createPropertyFrom
 import org.hexworks.zircon.api.data.Size3D
 
 /**
  * Options that change the behaviour of the game.
  */
 object GameOptions {
-    var attenuationModel = PercentageReducingAttenuation()
+    val attenuationModel = createPropertyFrom(PercentageReducingAttenuation())
 
     private const val SECTORS_COUNT_H = 10
     private const val SECTORS_COUNT_V = 10
     /**
      * The size of the complete world in tiles as [Size3D].
      */
-    val WORLD_SIZE: Size3D = Size3D.create(GameOptions.SECTORS_COUNT_H * Sector.TILE_COUNT, GameOptions.SECTORS_COUNT_V * Sector.TILE_COUNT, 1)
+    val WORLD_SIZE: Size3D = Size3D.create(SECTORS_COUNT_H * Sector.TILE_COUNT, SECTORS_COUNT_V * Sector.TILE_COUNT, 1)
 }
