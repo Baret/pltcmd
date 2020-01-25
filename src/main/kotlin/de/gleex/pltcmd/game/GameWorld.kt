d9fc2a907b0d23f0764aeb6bde5afa765654ca9b
Loom <looom@gmx.de>
2020-01-25 14:42:14 +0100

    flipped GameWorld in GameArea and fitted GameArea to GameWorld only
    So the game starts with its block position in the top left corner at 0,0 but
    the map grid starts in the bottom left corner at an arbitrary coordinate

--- src/main/kotlin/de/gleex/pltcmd/game/GameWorld.kt
+++ src/main/kotlin/de/gleex/pltcmd/game/GameWorld.kt
@@ -6,16 +6,16 @@ import de.gleex.pltcmd.model.terrain.TerrainType
 import de.gleex.pltcmd.model.world.Coordinate
 import de.gleex.pltcmd.model.world.Sector
 import de.gleex.pltcmd.model.world.WorldMap
-import de.gleex.pltcmd.options.GameOptions
 import org.hexworks.cobalt.logging.api.LoggerFactory
 import org.hexworks.zircon.api.Sizes
 import org.hexworks.zircon.api.builder.game.GameAreaBuilder
+import org.hexworks.zircon.api.data.Position
 import org.hexworks.zircon.api.data.Tile
 import org.hexworks.zircon.api.data.impl.Position3D
 import org.hexworks.zircon.api.game.GameArea
 
-class GameWorld(worldMap: WorldMap): GameArea<Tile, MapBlock> by GameAreaBuilder.newBuilder<Tile, MapBlock>().
-        withActualSize(Sizes.create3DSize(GameOptions.SECTORS_COUNT_H * Sector.TILE_COUNT, GameOptions.SECTORS_COUNT_V * Sector.TILE_COUNT, 1)).
+class GameWorld(worldMap: WorldMap) : GameArea<Tile, MapBlock> by GameAreaBuilder.newBuilder<Tile, MapBlock>().
+        withActualSize(Sizes.from2DTo3D(worldMap.size, 1)).
         withVisibleSize(Sizes.create3DSize(Sector.TILE_COUNT, Sector.TILE_COUNT, 1)).
         withLayersPerBlock(MapBlock.LAYERS_PER_BLOCK).
         withDefaultBlock(MapBlock(Terrain(TerrainType.GRASSLAND, TerrainHeight.ONE))).
@@ -25,30 +25,57 @@ class GameWorld(worldMap: WorldMap): GameArea<Tile, MapBlock> by GameAreaBuilder
         private val log = LoggerFactory.getLogger(GameWorld::class)
     }
 
+    // TODO only works if world is immutable
+    private val topLeftOffset = worldMap.getTopLeftOffset()
+
     init {
-        worldMap.sectors.forEach(::putSector)
+        toBlocks(worldMap)
         log.debug("Created GameWorld with ${worldMap.sectors.size} sectors. Visible size = ${visibleSize()}")
     }
 
-    private fun putSector(sector: Sector) {
-        sector.tiles.forEach {
-            val position = it.coordinate.toPosition()
-            val block = MapBlock(it.terrain)
-            setBlockAt(position, block)
+    private fun toBlocks(worldMap: WorldMap) {
+        worldMap.sectors.forEach { sector ->
+            sector.tiles.forEach {
+                val position = it.coordinate.toPosition()
+                val block = MapBlock(it.terrain)
+                setBlockAt(position, block)
+            }
         }
     }
-
-    private fun Coordinate.toPosition() = Position3D.create(eastingFromLeft, northingFromBottom, 0)
-
-    private fun Position3D.toCoordinate() = Coordinate(x, y)
 
     /** Returns the [Coordinate] of the [Tile] that is visible in the top left corner. */
     fun visibleTopLeftCoordinate(): Coordinate {
         return visibleOffset().toCoordinate()
     }
 
-    fun scrollToCoordinate(coord: Coordinate) {
-        scrollTo3DPosition(coord.toPosition())
+    fun scrollToCoordinate(bottomLeft: Coordinate) {
+        val bottomLeftPos = bottomLeft.toPosition()
+        val visibleTopLeftPos = bottomLeftPos.withRelativeY(-getMaxVisibleY())
+        scrollTo3DPosition(visibleTopLeftPos)
+    }
+
+    // model size
+    private fun getMaxY() = actualSize().yLength - 1 // -1 because y is zero-based
+
+    // ui size
+    private fun getMaxVisibleY() = visibleSize().yLength - 1 // -1 because y is zero-based
+
+    private fun WorldMap.getTopLeftOffset(): Position {
+        // we translate the world map coordinates which start with an arbitrary value to our game area coordinates which start with (0, 0)
+        // use minimal numeric value of coordinate to calculate the offset
+        return Position.create(-origin.eastingFromLeft, -origin.northingFromBottom)
+    }
+
+    private fun Coordinate.toPosition(): Position3D {
+        val translatedPos = Position.create(eastingFromLeft, northingFromBottom) + topLeftOffset
+        // invert y axis
+        return Position3D.from2DPosition(translatedPos.withY(getMaxY() - translatedPos.y))
+    }
+
+    private fun Position3D.toCoordinate(): Coordinate {
+        val translatedPos = to2DPosition() - topLeftOffset
+        // invert y axis
+        return Coordinate(translatedPos.x, translatedPos.y + getMaxY())
     }
 
 }
---
+++ src/test/kotlin/de/gleex/pltcmd/model/world/WorldMapTest.kt
@@ -0,0 +1,52 @@
+package de.gleex.pltcmd.model.world
+
+import org.hexworks.zircon.api.data.Size
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class WorldMapTest {
+
+    @Test
+    fun getSizeEmpty() {
+        val expected = Size.create(0, 0)
+        val result = WorldMap(emptySet()).size
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun getSizeSingle() {
+        val expected = Size.create(Sector.TILE_COUNT, Sector.TILE_COUNT)
+        val result = WorldMap(setOf(createSector(3))).size
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun getSizeCalculated() {
+        testSize(2)
+        testSize(3)
+        testSize(11)
+    }
+
+    private fun testSize(sideLengthInSectors: Int) {
+        val sectors = createSectors(sideLengthInSectors)
+        val expected = Size.create(sideLengthInSectors * Sector.TILE_COUNT, sideLengthInSectors * Sector.TILE_COUNT)
+
+        val result = WorldMap(sectors).size
+
+        assertEquals(expected, result)
+    }
+
+    fun createSectors(sideLength: Int): Set<Sector> {
+        val sectors = mutableSetOf<Sector>()
+        for (i in 0 until (sideLength * sideLength)) {
+            sectors.add(createSector(i))
+        }
+        return sectors
+    }
+
+    fun createSector(index: Int): Sector {
+        val origin = Coordinate(index * 50, index * 50)
+        return Sector.generateAt(origin)
+    }
+
+}
