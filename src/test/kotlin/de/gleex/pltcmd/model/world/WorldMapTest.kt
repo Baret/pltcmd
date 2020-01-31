2a04f8ccce56ecaa90f67138535f55ed9f0e781a
Baret <beret14@web.de>
2020-01-31 11:03:07 +0100

    removed unnessecary test body

--- src/test/kotlin/de/gleex/pltcmd/model/world/WorldMapTest.kt
+++ src/test/kotlin/de/gleex/pltcmd/model/world/WorldMapTest.kt
@@ -7,8 +7,6 @@ import io.kotlintest.shouldThrow
 import io.kotlintest.specs.WordSpec
 import io.kotlintest.tables.row
 import org.hexworks.zircon.api.data.Size
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
 
 class WorldMapTest: WordSpec({
     "A WorldMaps" should {
@@ -36,45 +34,7 @@ class WorldMapTest: WordSpec({
             }
         }
     }
-}) {
-
-    @Test
-    fun getSizeSingle() {
-        val expected = Size.create(Sector.TILE_COUNT, Sector.TILE_COUNT)
-        val result = WorldMap(setOf(createSector(3))).size
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun getSizeCalculated() {
-        testSize(2)
-        testSize(3)
-        testSize(11)
-    }
-
-    private fun testSize(sideLengthInSectors: Int) {
-        val sectors = createSectors(sideLengthInSectors)
-        val expected = Size.create(sideLengthInSectors * Sector.TILE_COUNT, sideLengthInSectors * Sector.TILE_COUNT)
-
-        val result = WorldMap(sectors).size
-
-        assertEquals(expected, result)
-    }
-
-    fun createSectors(sideLength: Int): Set<Sector> {
-        val sectors = mutableSetOf<Sector>()
-        for (i in 0 until (sideLength * sideLength)) {
-            sectors.add(createSector(i))
-        }
-        return sectors
-    }
-
-    fun createSector(index: Int): Sector {
-        val origin = Coordinate(index * 50, index * 50)
-        return Sector.generateAt(origin)
-    }
-
-}
+})
 
 private fun Int.sectors(): Set<Sector> {
         val sectors = mutableSetOf<Sector>()
