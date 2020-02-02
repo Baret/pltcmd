1f94df8c4536a326ddd2a35e1fe75910b3729d22
Loom <looom@gmx.de>
2020-02-02 09:21:29 +0100

    fixed the fix :D

--- src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/intermediate/MountainTopGenerator.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/intermediate/MountainTopGenerator.kt
@@ -6,7 +6,7 @@ import de.gleex.pltcmd.model.terrain.TerrainType
 import de.gleex.pltcmd.model.world.Coordinate
 
 class MountainTopGenerator(override val context: GenerationContext) : IntermediateGenerator {
-    fun generateArea(bottomLeftCoordinate: Coordinate, topRightCoordinate: Coordinate, terrainMap: Map<Coordinate, Pair<TerrainHeight?, TerrainType?>>, context: GenerationContext) {
+    override fun generateArea(bottomLeftCoordinate: Coordinate, topRightCoordinate: Coordinate, terrainMap: Map<Coordinate, Pair<TerrainHeight?, TerrainType?>>) {
         // pick random positions for mountain tops
         // set those to max height
         // from each position find the four neighbours that have no height yet
