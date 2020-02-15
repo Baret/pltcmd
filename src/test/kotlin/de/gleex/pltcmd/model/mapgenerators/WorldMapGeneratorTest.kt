881960b17554ac7f370d4641e8d04245d739956f
Baret <beret14@web.de>
2020-02-15 23:31:01 +0100

    building matcher for WorldMap

--- src/test/kotlin/de/gleex/pltcmd/model/mapgenerators/WorldMapGeneratorTest.kt
+++ src/test/kotlin/de/gleex/pltcmd/model/mapgenerators/WorldMapGeneratorTest.kt
@@ -1,8 +1,10 @@
 package de.gleex.pltcmd.model.mapgenerators
 
+import de.gleex.pltcmd.testhelpers.haveSameTerrain
+import de.gleex.pltcmd.testhelpers.shouldHaveSameTerrain
 import io.kotlintest.assertSoftly
 import io.kotlintest.matchers.types.shouldNotBeSameInstanceAs
-import io.kotlintest.shouldBe
+import io.kotlintest.shouldNot
 import io.kotlintest.specs.WordSpec
 
 class WorldMapGeneratorTest: WordSpec({
@@ -11,11 +13,11 @@ class WorldMapGeneratorTest: WordSpec({
     val height = 50
     val generatedWorld1 = WorldMapGenerator(seed, width, height).generateWorld()
     "The generator" should {
-        for(attempt in 1..500) {
+        for(attempt in 1..2) {
             "always generate the same world with the seed $seed (attempt $attempt)" {
                 val generatedWorld2 = WorldMapGenerator(seed, width, height).generateWorld()
                 assertSoftly {
-                    generatedWorld2 shouldBe generatedWorld1
+                    generatedWorld2 shouldHaveSameTerrain generatedWorld1
                     generatedWorld2 shouldNotBeSameInstanceAs generatedWorld1
                 }
             }
@@ -23,9 +25,9 @@ class WorldMapGeneratorTest: WordSpec({
 
         for(differentSeed in seed+1..seed+11) {
             "always generate a different world with different seed $differentSeed" {
-                val generatedWorld3 = WorldMapGenerator(seed, width, height).generateWorld()
+                val generatedWorld3 = WorldMapGenerator(differentSeed, width, height).generateWorld()
                 assertSoftly {
-                    generatedWorld3 shouldBe generatedWorld1
+                    generatedWorld3 shouldNot haveSameTerrain(generatedWorld1)
                     generatedWorld3 shouldNotBeSameInstanceAs generatedWorld1
                 }
             }
--- src/test/kotlin/de/gleex/pltcmd/testhelpers/Matchers.kt
+++ src/test/kotlin/de/gleex/pltcmd/testhelpers/Matchers.kt
@@ -1,5 +1,7 @@
 package de.gleex.pltcmd.testhelpers
 
+import de.gleex.pltcmd.model.world.WorldMap
+import de.gleex.pltcmd.model.world.WorldTile
 import io.kotlintest.Matcher
 import io.kotlintest.MatcherResult
 import io.kotlintest.should
@@ -29,4 +31,31 @@ fun beEmpty() = object: Matcher<Maybe<out Any>> {
     }
 }
 
-// - - -
+// - - - Matchers for WorldMap
+
+infix fun WorldMap.shouldHaveSameTerrain(other: WorldMap) = this should haveSameTerrain(other)
+
+fun haveSameTerrain(expected: WorldMap) = object: Matcher<WorldMap> {
+    override fun test(value: WorldMap): MatcherResult {
+        var errorMessage = ""
+        var isSame = true
+        val firstFail: WorldTile? = null
+
+        if(value.origin != expected.origin) {
+            errorMessage = "origin ${value.origin} does not equal expected ${expected.origin}"
+            isSame = false
+        } else if(value.size != expected.size) {
+            errorMessage = "size of ${value.size} does not equal expected size of ${expected.size}"
+            isSame = false
+        } else {
+            // check all tiles
+        }
+
+        return MatcherResult.Companion.invoke(
+                isSame,
+                "world map should have the same terrain but $errorMessage",
+                "world map should not have the same terrain"
+        )
+    }
+
+}
