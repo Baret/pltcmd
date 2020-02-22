4b06ce929422f2935c2e7eace9b15f88fd9a15a6
Loom <looom@gmx.de>
2020-02-22 09:15:45 +0100

    restructured RadioSignalFragment.kt

--- src/main/kotlin/de/gleex/pltcmd/ui/fragments/RadioSignalFragment.kt
+++ src/main/kotlin/de/gleex/pltcmd/ui/fragments/RadioSignalFragment.kt
@@ -2,6 +2,7 @@ package de.gleex.pltcmd.ui.fragments
 
 import de.gleex.pltcmd.game.TileRepository
 import de.gleex.pltcmd.model.radio.AbsoluteSignalLossAttenuation
+import de.gleex.pltcmd.model.radio.AttenuationModel
 import de.gleex.pltcmd.model.radio.PercentageReducingAttenuation
 import de.gleex.pltcmd.model.radio.RadioSignal
 import de.gleex.pltcmd.options.GameOptions
@@ -11,12 +12,15 @@ import org.hexworks.cobalt.databinding.api.extension.createPropertyFrom
 import org.hexworks.cobalt.databinding.api.property.Property
 import org.hexworks.zircon.api.Components
 import org.hexworks.zircon.api.Fragments
+import org.hexworks.zircon.api.builder.Builder
+import org.hexworks.zircon.api.component.*
+import org.hexworks.zircon.api.fragment.MultiSelect
 import kotlin.math.min
 
 /**
  * A fragment used to configure the settings for the RadioSignalVisualizer.
  */
-class RadioSignalFragment(override val width: Int): BaseFragment {
+class RadioSignalFragment(override val width: Int) : BaseFragment {
 
     val selectedStrength: Property<Int> = createPropertyFrom(100)
     val selectedRange: Property<Int> = createPropertyFrom(5)
@@ -27,72 +31,16 @@ class RadioSignalFragment(override val width: Int): BaseFragment {
             withSpacing(0).
             build().
             apply {
+                val header = buildHeader()
+                val legend = buildLegend()
 
-                val header = Components.
-                        header().
-                        withSize(this@RadioSignalFragment.width, 1).
-                        withText("Radio signal settings").
-                        build()
+                val strengthLabel = buildLabelLine()
+                val strengthInput = buildStrengthInputh(strengthLabel)
 
-                val legend = Components.
-                        hbox().
-                        withSpacing(0).
-                        withSize(this@RadioSignalFragment.width, 1).
-                        build().
-                        apply {
-                            addComponents(
-                                    Components.label().withText("100%"),
-                                    Components.icon().withIcon(TileRepository.forSignal(1.0)),
-                                    Components.label().withText(" ${RadioSignal.MIN_STRENGTH_THRESHOLD}%(min)"),
-                                    Components.icon().withIcon(TileRepository.forSignal(RadioSignal.MIN_STRENGTH_THRESHOLD / 100.0)),
-                                    Components.label().withText(" 0%"),
-                                    Components.icon().withIcon(TileRepository.forSignal(0.0))
-                            )
-                        }
+                val rangeLabel = buildLabelLine()
+                val rangeInput = buildRangeInput(rangeLabel)
 
-                val strengthLabel = Components.
-                        label().
-                        withSize(this@RadioSignalFragment.width, 1).
-                        build()
-                val strengthInput = Components.
-                        horizontalSlider().
-                        withMinValue(1).
-                        withMaxValue(1501).
-                        withNumberOfSteps(min(30, this@RadioSignalFragment.width)).
-                        withSize(this@RadioSignalFragment.width, 2).
-                        build()
-
-                strengthLabel.textProperty.updateFrom(createPropertyFrom("Strength: ") bindPlusWith strengthInput.currentValueProperty.bindToString())
-                selectedStrength.updateFrom(strengthInput.currentValueProperty)
-                strengthInput.currentValueProperty.value = 100
-
-                val rangeLabel = Components.
-                        label().
-                        withSize(this@RadioSignalFragment.width, 1).
-                        build()
-                val rangeInput = Components.
-                        horizontalSlider().
-                        withMinValue(1).
-                        withMaxValue(70).
-                        withNumberOfSteps(min(34, this@RadioSignalFragment.width)).
-                        build()
-
-                rangeLabel.textProperty.updateFrom(createPropertyFrom("Range: ") bindPlusWith rangeInput.currentValueProperty.bindToString())
-                selectedRange.updateFrom(rangeInput.currentValueProperty)
-                rangeInput.currentValueProperty.value = 5
-
-                val modelLabel = Components.
-                        label().
-                        withText("Attenuation model").
-                        build()
-
-                val models = listOf(Pair("by percentage", PercentageReducingAttenuation()), Pair("absolute", AbsoluteSignalLossAttenuation()))
-
-                val modelInput = Fragments.
-                        multiSelect(this@RadioSignalFragment.width, models).
-                        withCallback { _, newValue -> GameOptions.attenuationModel.value = newValue.second }.
-                        withToStringMethod { it.first }.
-                        build()
+                val modelLabel = buildModelLabel()
 
                 addComponents(
                         header,
@@ -103,6 +51,91 @@ class RadioSignalFragment(override val width: Int): BaseFragment {
                         rangeInput,
                         modelLabel
                 )
+
+                val modelInput = buildModelInput()
                 addFragment(modelInput)
             }
+
+    private fun buildHeader(): Header {
+        return Components.
+                header().
+                withText("Radio signal settings").
+                build()
+    }
+
+    private fun buildLegend(): HBox {
+        return Components.
+                hbox().
+                withSpacing(0).
+                withSize(width, 1).
+                build().
+                apply {
+                    addComponents(*buildLegendText())
+                }
+    }
+
+    private fun buildLegendText(): Array<Builder<Component>> {
+        return arrayOf(
+                Components.label().withText("100%"),
+                Components.icon().withIcon(TileRepository.forSignal(1.0)),
+                Components.label().withText(" ${RadioSignal.MIN_STRENGTH_THRESHOLD}%(min)"),
+                Components.icon().withIcon(TileRepository.forSignal(RadioSignal.MIN_STRENGTH_THRESHOLD / 100.0)),
+                Components.label().withText(" 0%"),
+                Components.icon().withIcon(TileRepository.forSignal(0.0))
+        )
+    }
+
+    private fun buildStrengthInputh(strengthLabel: Label): Slider {
+        val strengthInput = Components.
+                horizontalSlider().
+                withMinValue(1).
+                withMaxValue(1501).
+                withNumberOfSteps(min(30, width)).
+                withSize(width, 2).
+                build().
+                apply {
+                    currentValue = selectedStrength.value
+                    selectedStrength.updateFrom(currentValueProperty)
+                }
+
+        strengthLabel.textProperty.updateFrom(createPropertyFrom("Strength: ") bindPlusWith strengthInput.currentValueProperty.bindToString())
+        return strengthInput
+    }
+
+    private fun buildLabelLine(text : String = ""): Label {
+        return Components.
+                label().
+                withText(text).
+                withSize(width, 1).
+                build()
+    }
+
+    private fun buildRangeInput(rangeLabel: Label): Slider {
+        val rangeInput = Components.
+                horizontalSlider().
+                withMinValue(1).
+                withMaxValue(70).
+                withNumberOfSteps(min(34, width)).
+                build().
+                apply {
+                    currentValueProperty.value = selectedRange.value
+                    selectedRange.updateFrom(currentValueProperty)
+                }
+
+        rangeLabel.textProperty.updateFrom(createPropertyFrom("Range: ") bindPlusWith rangeInput.currentValueProperty.bindToString())
+        return rangeInput
+    }
+
+    private fun buildModelLabel(): Label = buildLabelLine("Attenuation model")
+
+    private fun buildModelInput(): MultiSelect<Pair<String, AttenuationModel>> {
+        val models = listOf(Pair("by percentage", PercentageReducingAttenuation()), Pair("absolute", AbsoluteSignalLossAttenuation()))
+
+        return Fragments.
+                multiSelect(width, models).
+                withCallback { _, newValue -> GameOptions.attenuationModel.value = newValue.second }.
+                withToStringMethod { it.first }.
+                build()
+    }
+
 }
