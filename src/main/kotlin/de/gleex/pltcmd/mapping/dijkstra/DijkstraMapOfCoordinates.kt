cba59ad1542f685322024f3c2e5a9d1fb6657f8f
Baret <beret14@web.de>
2020-02-25 12:56:42 +0100

    first implementation of the new dijkstra map, unit test needed

--- src/main/kotlin/de/gleex/pltcmd/mapping/dijkstra/DijkstraMapOfCoordinates.kt
+++ src/main/kotlin/de/gleex/pltcmd/mapping/dijkstra/DijkstraMapOfCoordinates.kt
@@ -4,6 +4,7 @@ import de.gleex.pltcmd.model.world.Coordinate
 
 /**
  * not yet implemented!
+ * TODO: Fix documentation
  */
 class DijkstraMapOfCoordinates(private val map: Map<Coordinate, Int>) {
     // This should become a better implementation of a dijkstra map
@@ -17,34 +18,55 @@ class DijkstraMapOfCoordinates(private val map: Map<Coordinate, Int>) {
 
     private val targets = map.filterValues { it == 0 }.keys
 
+    /**
+     * Calculates the path from the given coordinate to a desired value (default 0) along this map.
+     * If the coordinate is not present in this map an empty sequence is returned. If it already
+     * has the given [targetValue] a sequence containing only this coordinate is returned.
+     */
     fun pathFrom(from: Coordinate, targetValue: Int = 0): Sequence<Coordinate> {
         val startValue = map[from]
-        return if(startValue == null) {
-            emptySequence()
-        } else if(startValue == targetValue) {
-            return sequenceOf(from)
-        } else if(startValue > targetValue) {
-            downstream(from, targetValue)
-        } else {
-            emptySequence()
-//            upstream(from, targetValue)
-        }
-    }
-
-    private fun downstream(from: Coordinate, targetValue: Int): Sequence<Coordinate> {
-        var next: Coordinate? = from
-        return sequence {
-            if(next != null) {
-                yield(next!!)
-                // TODO: find the neighbor with the lowest value in [map]
-                next = neighborsOf(next!!).
-                        filter { map.containsKey(it) }.
-                        firstOrNull()
+        return when {
+            startValue == null        -> {
+                emptySequence()
+            }
+            startValue == targetValue -> {
+                sequenceOf(from)
+            }
+            startValue > targetValue  -> {
+                downstream(from, targetValue)
+            }
+            else                      -> {
+                upstream(from, targetValue)
             }
         }
     }
 
-    private fun neighborsOf(coordinate: Coordinate): List<Coordinate> {
-        return coordinate.neighbors()
+    private fun downstream(from: Coordinate, targetValue: Int) =
+            internalPath(from, targetValue, true)
+
+    private fun upstream(from: Coordinate, targetValue: Int) =
+            internalPath(from, targetValue, false)
+
+    private fun internalPath(from: Coordinate, targetValue: Int, downstream: Boolean): Sequence<Coordinate> {
+        var next: Coordinate? = from
+        var currentValue = map[from]
+        return sequence {
+            if(next != null || currentValue == targetValue) {
+                yield(next!!)
+                val allNeighbors = neighborsOf(next!!).
+                        map { it to map[it] }.
+                        filter{ it.second != null }
+                val nextNeighbor = if(downstream) {
+                            allNeighbors.minBy { it.second!! }
+                        } else {
+                            allNeighbors.maxBy { it.second!! }
+                        }
+                next = nextNeighbor?.
+                        also { currentValue = it.second!! }?.
+                        first
+            }
+        }
     }
+
+    private fun neighborsOf(coordinate: Coordinate) = coordinate.neighbors()
 }
