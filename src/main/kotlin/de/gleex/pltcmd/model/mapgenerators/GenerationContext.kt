40f86438b2cb4c081767676c6fd498e2d02a2f11
Loom <looom@gmx.de>
2020-03-01 12:46:48 +0100

    first part of PlainsGenerator

--- src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/GenerationContext.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/GenerationContext.kt
@@ -8,14 +8,29 @@ import kotlin.random.Random
  * A context for generating an area of the map. Contains values from 0 to 1 (exclusive) for different attributes for the map.
  */
 data class GenerationContext(
+        val plains: Double,
         val forest: Double,
         val mountain: Double,
         val water: Double,
-        val urban: Double
+        val urban: Double,
+        /** open world to glue specific parts together */
+        val undefined: Double
 ) {
     companion object {
-        fun fromRandom(random: Random) = GenerationContext(random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble())
+        fun fromRandom(random: Random) = GenerationContext(random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble(), random.nextDouble())
     }
 
+    val plainsRatio: Double = asRatio(plains)
+    val forestRatio: Double = asRatio(forest)
+    val mountainRatio: Double = asRatio(mountain)
+    val waterRatio: Double = asRatio(water)
+    val urbanRatio: Double = asRatio(urban)
+    val undefinedRatio: Double = asRatio(undefined)
+
     val mountainTops = DijkstraMap<Coordinate>()
+
+    private fun asRatio(value: Double): Double {
+        return (plains + forest + mountain + water + urban) / value
+    }
+
 }
---
+++ src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/intermediate/PlainsGenerator.kt
@@ -0,0 +1,52 @@
+package de.gleex.pltcmd.model.mapgenerators.intermediate
+
+import de.gleex.pltcmd.model.mapgenerators.GenerationContext
+import de.gleex.pltcmd.model.mapgenerators.data.MutableWorld
+import de.gleex.pltcmd.model.terrain.Terrain
+import de.gleex.pltcmd.model.terrain.TerrainHeight
+import de.gleex.pltcmd.model.terrain.TerrainType
+import de.gleex.pltcmd.model.world.CoordinateArea
+import kotlin.math.roundToInt
+import kotlin.random.Random
+
+/**
+ * Generates multiple plains in an area. Plains are rectangles of grass land with a height between two to five. They have a minimum height/width of at least 5 tiles and a maximum of 50. The edges fade out randomly.
+ */
+class PlainsGenerator(override val rand: Random, override val context: GenerationContext) : IntermediateGenerator() {
+
+    companion object {
+        private const val MIN_WIDTH = 5  // tiles
+        private const val MAX_WIDTH = 50 // tiles
+    }
+
+
+    override fun generateArea(area: CoordinateArea, mutableWorld: MutableWorld) {
+        val totalPlainsTiles = totalTiles(area)
+        val plainsRectangles = findPlainsLocations(totalPlainsTiles, area, mutableWorld)
+        plainsRectangles.forEach {
+            generatePlains(it, mutableWorld)
+        }
+    }
+
+    private fun totalTiles(area: CoordinateArea): Int {
+        return (area.size * context.plainsRatio).roundToInt()
+    }
+
+    private fun findPlainsLocations(totalPlainsTiles: Int, area: CoordinateArea, mutableWorld: MutableWorld): Set<CoordinateArea> {
+        return TODO()
+    }
+
+    private fun generatePlains(area: CoordinateArea, mutableWorld: MutableWorld) {
+        val plainsHeight = listOf<TerrainHeight>(
+                TerrainHeight.TWO,
+                TerrainHeight.THREE,
+                TerrainHeight.FOUR,
+                TerrainHeight.FIVE)
+                .random(rand)
+        val plains = Terrain.of(TerrainType.GRASSLAND, plainsHeight)
+        for (coordinate in area) {
+            mutableWorld[coordinate] = plains
+        }
+    }
+
+}
