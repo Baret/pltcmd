cbf040c92a71e46051d38b0413a1c085ce1600f7
Loom <looom@gmx.de>
2020-03-06 00:14:01 +0100

    improved performance of EmptyRectangleAreaFinder by just looking at the next empty tile
    instead of creating new Coordinates and CoordinateAreas and calling containsAll()

--- src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/areafinder/EmptyRectangleAreaFinder.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/areafinder/EmptyRectangleAreaFinder.kt
@@ -2,7 +2,6 @@ package de.gleex.pltcmd.model.mapgenerators.areafinder
 
 import de.gleex.pltcmd.model.mapgenerators.data.MutableWorld
 import de.gleex.pltcmd.model.world.Coordinate
-import de.gleex.pltcmd.model.world.CoordinateArea
 import de.gleex.pltcmd.model.world.CoordinateRectangle
 import org.hexworks.cobalt.logging.api.LoggerFactory
 
@@ -15,40 +14,62 @@ class EmptyRectangleAreaFinder : AreaFinder {
     }
 
     override fun findAll(partialWorld: MutableWorld): Set<CoordinateRectangle> {
+        val start = System.currentTimeMillis()
         val rectangles = mutableSetOf<CoordinateRectangle>()
         val allEmpty = partialWorld.findEmpty()
                 .sorted()
                 .toMutableList()
         while (allEmpty.isNotEmpty()) {
-            val start = allEmpty.removeAt(0)
-            val rectangle = getEmptyRectangle(start, allEmpty)
+            val rectangle = getEmptyRectangle(allEmpty)
             allEmpty.removeAll(rectangle.toSet())
             rectangles.add(rectangle)
         }
-        LOG.debug("Empty rectangles found in world: $rectangles")
+        val duration = System.currentTimeMillis() - start
+        LOG.debug("Found ${rectangles.size} empty rectangles in $duration ms")
         return rectangles
     }
 
-    private fun getEmptyRectangle(start: Coordinate, allEmpty: Collection<Coordinate>): CoordinateRectangle {
+    private fun getEmptyRectangle(followingEmpty: List<Coordinate>): CoordinateRectangle {
+//        val startTime = System.currentTimeMillis()
+        val start = followingEmpty[0]
         // prefer horizontal connected tiles (lines)
-        var width = 1
-        while (start.withRelativeEasting(width) in allEmpty) {
-            width++
-        }
+        val width = getConnectedLineLength(start, followingEmpty)
         // check all vertical tiles (lines of same length)
         var height = 1
-        while (isEmptyLine(start.withRelativeNorthing(height), width, allEmpty)) {
-            height++
+        if (width > 1) {
+            while (getConnectedLineLength(start.withRelativeNorthing(height), followingEmpty) == width) {
+                height++
+            }
         }
         val end = start.withRelativeEasting(width - 1)
                 .withRelativeNorthing(height - 1)
-        LOG.trace("Found rectangle from $start to $end ($width x $height)")
+//        val duration = System.currentTimeMillis() - startTime
+//        LOG.debug("Found rectangle from $start to $end ($width x $height) in $duration ms or ${duration / (width * height)} ms per tile")
         return CoordinateRectangle(start, end)
     }
 
-    private fun isEmptyLine(lineStart: Coordinate, width: Int, allEmpty: Collection<Coordinate>): Boolean {
-        val line = CoordinateArea(lineStart..lineStart.withRelativeEasting(width - 1))
-        return allEmpty.containsAll(line.toSet())
+    private fun getConnectedLineLength(start: Coordinate, followingEmpty: List<Coordinate>): Int {
+        val startIndex = followingEmpty.indexOf(start)
+        if (startIndex == -1) {
+            // element behind all empty coordinates
+            return 0
+        }
+        var indexToCheck = startIndex + 1
+        var previous = start
+        while (indexToCheck < followingEmpty.size) {
+            val next = followingEmpty[indexToCheck]
+            if (next.followsHorizontally(previous)) {
+                indexToCheck++
+                previous = next
+            } else {
+                break
+            }
+        }
+        return indexToCheck - startIndex
+    }
+
+    private fun Coordinate.followsHorizontally(previous: Coordinate): Boolean {
+        return previous.eastingFromLeft + 1 == eastingFromLeft
     }
 
 }
