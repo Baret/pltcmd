8f8bc6ccf18908d81743e40cc366a5b03766230d
Loom <looom@gmx.de>
2020-03-14 20:12:17 +0100

    Update UI asynchronous every now and then

--- src/main/kotlin/de/gleex/pltcmd/ProgressListener.kt
+++ src/main/kotlin/de/gleex/pltcmd/ProgressListener.kt
@@ -5,22 +5,27 @@ import de.gleex.pltcmd.model.terrain.TerrainHeight
 import de.gleex.pltcmd.model.terrain.TerrainType
 import de.gleex.pltcmd.model.world.Coordinate
 import org.hexworks.cobalt.databinding.api.property.Property
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 /** Listener that updates a property to the relative number of already generated map tiles. */
 class ProgressListener(totalTiles: Int, private val maxProgress: Double, private val progressToSet: Property<Double>) : MapGenerationListener {
     private val terrainToGenerate = totalTiles * 2 // 2 = height and type
     private val generatedHeights = mutableSetOf<Coordinate>()
     private val generatedTypes = mutableSetOf<Coordinate>()
+    private val scheduledUpdates = Executors.newSingleThreadScheduledExecutor()
+    val currentProgressValue: Double
+        get() = (generatedHeights.size + generatedTypes.size) * maxProgress / terrainToGenerate
+
+    init {
+        scheduledUpdates.scheduleAtFixedRate(Runnable {
+            progressToSet.updateValue(currentProgressValue)
+        }, 100L, 100L, TimeUnit.MILLISECONDS)
+    }
 
     override fun terrainGenerated(coordinate: Coordinate, terrainHeight: TerrainHeight?, terrainType: TerrainType?) {
-        var notYetProcessed = false
-        terrainHeight?.let { notYetProcessed = generatedHeights.add(coordinate) }
-        terrainType?.let { notYetProcessed = generatedTypes.add(coordinate) || notYetProcessed }
-
-        if (notYetProcessed) {
-            val newValue = (generatedHeights.size + generatedTypes.size) * maxProgress / terrainToGenerate
-            progressToSet.updateValue(newValue)
-        }
+        generatedHeights.add(coordinate)
+        generatedTypes.add(coordinate)
     }
 
 }
