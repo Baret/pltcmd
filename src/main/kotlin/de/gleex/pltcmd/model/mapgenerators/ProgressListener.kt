201040184a2f8ad84dcf08f1d119bca4d3f762b3
Loom <looom@gmx.de>
2020-03-19 10:00:56 +0100

    KDoc for parameters of ProgressListener

--- src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/ProgressListener.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/ProgressListener.kt
@@ -7,7 +7,13 @@ import org.hexworks.cobalt.databinding.api.property.Property
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
-/** Listener that updates a property to the relative number of already generated map tiles. */
+/**
+ * Listener that updates a property to the relative number of already generated map tiles.
+ *
+ * @param totalTiles this amount of events will be counted as 100% and is used to derive the relative progress
+ * @param maxProgress the highest value for the given [progressToSet] which corresponds to 100%
+ * @param progressToSet a property whose value reflects the progress. Its value will be set based on the relative progress and the absolute [maxProgress]
+ **/
 class ProgressListener(totalTiles: Int, private val maxProgress: Double, private val progressToSet: Property<Double>) : MapGenerationListener {
     private val terrainToGenerate = totalTiles * 2 // 2 = height and type
     private val generatedHeights = mutableSetOf<Coordinate>()
