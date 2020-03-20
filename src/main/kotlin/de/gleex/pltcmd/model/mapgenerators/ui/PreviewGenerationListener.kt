d4cf2b4fe4812ff17b2672067be6a73cebae85fd
Loom <looom@gmx.de>
2020-03-20 08:34:17 +0100

    Refactoring and KDoc for PreviewGenerationListener and AverageTerrain

--- src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/ui/PreviewGenerationListener.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/ui/PreviewGenerationListener.kt
@@ -1,6 +1,5 @@
 package de.gleex.pltcmd.model.mapgenerators.ui
 
-import de.gleex.pltcmd.game.GameWorld
 import de.gleex.pltcmd.model.mapgenerators.MapGenerationListener
 import de.gleex.pltcmd.model.terrain.AverageTerrain
 import de.gleex.pltcmd.model.terrain.TerrainHeight
@@ -8,7 +7,7 @@ import de.gleex.pltcmd.model.terrain.TerrainType
 import de.gleex.pltcmd.model.world.Coordinate
 import org.hexworks.zircon.api.data.Position3D
 
-/** Shows the current state of the world in a single sector sized [GameWorld] while the map is beeing generated. */
+/** Updates an [IncompleteMapGameArea] to show the current state of map generation. The map to be generated is scaled down to the size of the given [previewWorld]. */
 class PreviewGenerationListener(generatedWorldWidth: Int, generatedWorldHeight: Int, private val previewWorld: IncompleteMapGameArea) : MapGenerationListener {
     private val displaySize = previewWorld.actualSize.to2DSize()
     private val tilesPerPreviewWidth = generatedWorldWidth / displaySize.width
@@ -17,18 +16,18 @@ class PreviewGenerationListener(generatedWorldWidth: Int, generatedWorldHeight: 
     private val averageTerrainMap: MutableMap<Position3D, AverageTerrain> = mutableMapOf()
 
     override fun terrainGenerated(coordinate: Coordinate, terrainHeight: TerrainHeight?, terrainType: TerrainType?) {
-        val previewCoordinate = mapToPreview(coordinate)
+        val gameAreaPosition = coordinate.toGameAreaPosition()
 
-        val terrainData = averageTerrainMap.getOrPut(previewCoordinate, { AverageTerrain() })
-        terrainData.mappedTiles[coordinate] = Pair(terrainHeight, terrainType)
+        val terrainData = averageTerrainMap.getOrPut(gameAreaPosition, { AverageTerrain() })
+        terrainData.put(coordinate, terrainHeight, terrainType)
 
-        previewWorld.setBlock(previewCoordinate, terrainData.getAverageHeight(), terrainData.getDominatingType())
+        previewWorld.setBlock(gameAreaPosition, terrainData.getAverageHeight(), terrainData.getDominatingType())
     }
 
-    /** Divides the difference between the real and preview coordinate by the amount of aggregated tiles */
-    private fun mapToPreview(coordinate: Coordinate): Position3D {
-        val x = coordinate.eastingFromLeft / tilesPerPreviewWidth
-        val y = coordinate.northingFromBottom / tilesPerPreviewHeight
+    /** Maps the coordinate to the position in the preview. */
+    private fun Coordinate.toGameAreaPosition(): Position3D {
+        val x = eastingFromLeft / tilesPerPreviewWidth
+        val y = northingFromBottom / tilesPerPreviewHeight
         // invert y axis to map northing of coordinate to view y
         return Position3D.create(x, displaySize.height - y, 0)
     }
--- src/main/kotlin/de/gleex/pltcmd/model/terrain/AverageTerrain.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/terrain/AverageTerrain.kt
@@ -5,19 +5,19 @@ import java.util.*
 import java.util.stream.Collectors
 
 /** Holds all real world data that is mapped onto a single preview tile */
-data class AverageTerrain(val mappedTiles: MutableMap<Coordinate, Pair<TerrainHeight?, TerrainType?>> = mutableMapOf()) {
+data class AverageTerrain(private val mappedTiles: MutableMap<Coordinate, TerrainData> = mutableMapOf()) {
 
-    @Suppress("MemberVisibilityCanBePrivate")
+    data class TerrainData(var height: TerrainHeight?, var type: TerrainType?)
+
     fun getAverageHeight(): TerrainHeight? {
         return mappedTiles.values
-                .mapNotNull { it.first }
+                .mapNotNull { it.height }
                 .average()
     }
 
-    @Suppress("MemberVisibilityCanBePrivate")
     fun getDominatingType(): TerrainType? {
         return mappedTiles.values.stream()
-                .map { it.second }                  // get TerrainType
+                .map { it.type }                    // get TerrainType
                 .filter(Objects::nonNull)           // filter not yet generated
                 .collect(Collectors.groupingBy<TerrainType?, TerrainType?> { it })  // collect same types
                 .mapValues { it.value.size }        // count same types
@@ -25,15 +25,10 @@ data class AverageTerrain(val mappedTiles: MutableMap<Coordinate, Pair<TerrainHe
                 ?.key                               // which may be null
     }
 
-    fun createTerrain(): Terrain? {
-        val averageHeight = getAverageHeight()
-        val dominatingType = getDominatingType()
-        if (averageHeight == null && dominatingType == null) {
-            return null
-        }
-        val type = dominatingType ?: TerrainType.WATER_DEEP
-        val height = averageHeight ?: TerrainHeight.MIN
-        return Terrain.of(type, height)
+    fun put(coordinate: Coordinate, terrainHeight: TerrainHeight?, terrainType: TerrainType?) {
+        val terrainData = mappedTiles.computeIfAbsent(coordinate) { TerrainData(null, null) }
+        terrainData.height = terrainHeight
+        terrainData.type = terrainType
     }
 
 }
