519c739c0c40bfc30ae407d76fe143655bf078e7
Loom <looom@gmx.de>
2020-03-21 12:12:08 +0100

    create full map in initialization of IncompleteMapGameArea
    Before a new Map was created for every Block.

--- src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/ui/IncompleteMapGameArea.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/ui/IncompleteMapGameArea.kt
@@ -9,25 +9,28 @@ import org.hexworks.zircon.api.data.Tile
 import org.hexworks.zircon.api.game.base.BaseGameArea
 import org.hexworks.zircon.internal.game.impl.TopDownProjectionStrategy
 
+/**
+ * A [GameArea] that shows a map with holes. It uses [IncompleteMapBlock] for that. The full map is visible.
+ */
 class IncompleteMapGameArea(size: Size) :
         BaseGameArea<Tile, IncompleteMapBlock>(
                 initialVisibleSize = Size3D.from2DSize(size, 1),
                 initialActualSize = Size3D.from2DSize(size, 1),
-                initialContents = mapOf(),
+                initialContents = initialContents(size.width, size.height),
                 projectionStrategy = TopDownProjectionStrategy()) {
 
-    init {
-        // fill area with voidness
-        for (y in 0 until size.height) {
-            for (x in 0 until size.width) {
-                initBlock(x, y)
+    companion object {
+        private fun initialContents(width: Int, height: Int): Map<Position3D, IncompleteMapBlock> {
+            val contents = mutableMapOf<Position3D, IncompleteMapBlock>()
+            // fill area with voidness
+            for (y in 0 until height) {
+                for (x in 0 until width) {
+                    val position = Position3D.create(x, y, 0)
+                    contents[position] = IncompleteMapBlock()
+                }
             }
+            return contents
         }
-    }
-
-    private fun initBlock(x: Int, y: Int) {
-        val position = Position3D.create(x, y, 0)
-        setBlockAt(position, IncompleteMapBlock())
     }
 
     fun setBlock(position: Position3D, terrainHeight: TerrainHeight?, terrainType: TerrainType?) {
