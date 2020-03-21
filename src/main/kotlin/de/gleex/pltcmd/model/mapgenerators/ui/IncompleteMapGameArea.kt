ad9656162d308c0def19677ede98a10f52a56cfc
Loom <looom@gmx.de>
2020-03-21 12:15:35 +0100

    extended KDoc

--- src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/ui/IncompleteMapGameArea.kt
+++ src/main/kotlin/de/gleex/pltcmd/model/mapgenerators/ui/IncompleteMapGameArea.kt
@@ -6,11 +6,12 @@ import org.hexworks.zircon.api.data.Position3D
 import org.hexworks.zircon.api.data.Size
 import org.hexworks.zircon.api.data.Size3D
 import org.hexworks.zircon.api.data.Tile
+import org.hexworks.zircon.api.game.GameArea
 import org.hexworks.zircon.api.game.base.BaseGameArea
 import org.hexworks.zircon.internal.game.impl.TopDownProjectionStrategy
 
 /**
- * A [GameArea] that shows a map with holes. It uses [IncompleteMapBlock] for that. The full map is visible.
+ * A [GameArea] that shows a map with holes. It uses [IncompleteMapBlock] for that. The full map is visible and can be altered.
  */
 class IncompleteMapGameArea(size: Size) :
         BaseGameArea<Tile, IncompleteMapBlock>(
