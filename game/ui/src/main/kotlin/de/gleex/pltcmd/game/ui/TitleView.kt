a508091b8e89e650487ed9222155786edff76e24
Baret <beret14@web.de>
2020-04-22 17:40:02 +0200

    Use Layer instead of Component (with loads of other Components), needs work though

--- game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/TitleView.kt
+++ game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/TitleView.kt
@@ -2,18 +2,13 @@ package de.gleex.pltcmd.game.ui
 
 import de.gleex.pltcmd.game.options.UiOptions
 import de.gleex.pltcmd.ui.composites.GameLogo
-import org.hexworks.zircon.api.data.Size
 import org.hexworks.zircon.api.grid.TileGrid
 import org.hexworks.zircon.api.view.base.BaseView
-import kotlin.math.roundToInt
 
 /** Displays the title of the game. */
 class TitleView(tileGrid: TileGrid) : BaseView(theme = UiOptions.THEME, tileGrid = tileGrid) {
 
-    private val size = Size.create(tileGrid.width, (tileGrid.width / 1.6).roundToInt())
-
     init {
-        println("creating gamelogo with size $size to tilegrid with size ${tileGrid.size}")
-        screen.addFragment(GameLogo(size, screen))
+        GameLogo.drawOnto(screen)
     }
 }
--- game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/composites/GameLogo.kt
+++ game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/composites/GameLogo.kt
@@ -1,16 +1,14 @@
 package de.gleex.pltcmd.ui.composites
 
 import de.gleex.pltcmd.game.options.UiOptions
-import org.hexworks.zircon.api.Components
+import org.hexworks.zircon.api.CharacterTileStrings
 import org.hexworks.zircon.api.Modifiers
+import org.hexworks.zircon.api.builder.graphics.LayerBuilder
 import org.hexworks.zircon.api.color.ANSITileColor
-import org.hexworks.zircon.api.component.Component
-import org.hexworks.zircon.api.component.ComponentAlignment
-import org.hexworks.zircon.api.component.Container
-import org.hexworks.zircon.api.component.Fragment
 import org.hexworks.zircon.api.data.Position
 import org.hexworks.zircon.api.data.Size
 import org.hexworks.zircon.api.data.Tile
+import org.hexworks.zircon.api.graphics.Layer
 import org.hexworks.zircon.api.graphics.StyleSet
 import org.hexworks.zircon.api.graphics.Symbols
 import org.hexworks.zircon.api.grid.TileGrid
@@ -18,22 +16,26 @@ import org.hexworks.zircon.api.shape.LineFactory
 import kotlin.math.roundToInt
 
 /**
- * The first idea was to implement a game logo that draws some lines...
+ * The logo used as splash screen. Intended to be drawn onto the full screen, but might also be used in other places
  */
-class GameLogo(size: Size, parent: TileGrid) : Fragment {
+object GameLogo {
     private val style = StyleSet.create(UiOptions.THEME.primaryForegroundColor, ANSITileColor.BLACK)
     private val styleFadingShadow = style
             .withModifiers(Modifiers.fadeIn(20, 2000, true))
             .withForegroundColor(UiOptions.THEME.primaryBackgroundColor)
             .withBackgroundColor(ANSITileColor.BLACK)
 
-    override val root: Component = Components
-            .panel()
-            .withSize(size)
-            .withAlignmentWithin(parent, ComponentAlignment.CENTER)
-            .build()
+    fun drawOnto(parent: TileGrid) {
+        val size = Size.create(parent.width, (parent.width / 1.6).roundToInt())
 
-    init {
+        val mainLayer = LayerBuilder.newBuilder()
+                .withFiller(Tile.createCharacterTile(' ', style))
+//                .withTileset(UiOptions.DEFAULT_TILESET)
+                .withSize(size)
+                .withOffset(0, (parent.height - size.height) / 2)
+                .build()
+
+        parent.addLayer(mainLayer)
 
         val horizontal = size.width / 2
         val distanceFromBorder = (size.height * 0.3).roundToInt()
@@ -43,55 +45,56 @@ class GameLogo(size: Size, parent: TileGrid) : Fragment {
         val topLeftCorner = Position.zero()
         val bottomRightCorner = Position.create(size.width - 1, size.height - 1)
 
-        drawMainLine(topLeftCorner, lowerPoint, upperPoint, bottomRightCorner)
+        mainLayer.drawMainLine(topLeftCorner, lowerPoint, upperPoint, bottomRightCorner)
 
-        (root as Container).addComponent(Components.header()
-                .withText("p l t")
-                .withPosition(Position.create(horizontal - 6, size.height / 2)))
-        root.addComponent(Components.header()
-                .withText("c m d")
-                .withPosition(Position.create(horizontal + 3, size.height / 2)))
+        mainLayer.draw(
+                CharacterTileStrings
+                        .newBuilder()
+                        .withText("p l t")
+                        .build(),
+                Position.create(horizontal - 6, size.height / 2))
+
+        mainLayer.draw(
+                CharacterTileStrings
+                        .newBuilder()
+                        .withText("c m d")
+                        .build(),
+                Position.create(horizontal + 3, size.height / 2))
     }
 
-    private fun drawMainLine(topLeftCorner: Position, lowerPoint: Position, upperPoint: Position, bottomRightCorner: Position) {
+    private fun Layer.drawMainLine(topLeftCorner: Position, lowerPoint: Position, upperPoint: Position, bottomRightCorner: Position) {
         val leftDiagonalLine = LineFactory.buildLine(topLeftCorner, lowerPoint)
         val rightDiagonalLine = LineFactory.buildLine(upperPoint, bottomRightCorner)
         val verticalLine = LineFactory.buildLine(upperPoint, lowerPoint)
         val diagonals = leftDiagonalLine.plus(rightDiagonalLine)
         (diagonals + verticalLine).positions.forEach {
-            it.addBlock()
+            addBlockAt(it)
         }
 
         // add shadow above
         diagonals.positions.forEach {
             if (it.y > 0 && it.x != lowerPoint.x) {
-                it.addShadowVertical()
+                addShadowVertical(it)
             }
         }
 
         // add shadow right
         verticalLine.positions.forEach {
             if (it.y > upperPoint.y + 1) {
-                it.addShadowHorizontal()
+                addShadowHorizontal(it)
             }
         }
     }
 
-    private fun Position.addShadowVertical() = setShadow(this.withRelativeY(-1), Symbols.LOWER_HALF_BLOCK)
+    private fun Layer.addShadowVertical(pos: Position) = setShadow(pos.withRelativeY(-1), Symbols.LOWER_HALF_BLOCK)
 
-    private fun Position.addShadowHorizontal() = setShadow(this.withRelativeX(1), Symbols.LEFT_HALF_BLOCK)
+    private fun Layer.addShadowHorizontal(pos: Position) = setShadow(pos.withRelativeX(1), Symbols.LEFT_HALF_BLOCK)
 
-    private fun setShadow(position: Position, symbol: Char) = addTileAt(position, styleFadingShadow, symbol)
+    private fun Layer.setShadow(position: Position, symbol: Char) = addTileAt(position, styleFadingShadow, symbol)
 
-    private fun Position.addBlock() = addTileAt(this, style, Symbols.BLOCK_SOLID)
+    private fun Layer.addBlockAt(pos: Position) = this.addTileAt(pos, style, Symbols.BLOCK_SOLID)
 
-    private fun addTileAt(position: Position, style: StyleSet, symbol: Char) {
-        val tile = tileAt(position, style, symbol)
-        (root as Container).addComponent(tile)
+    private fun Layer.addTileAt(position: Position, style: StyleSet, symbol: Char) {
+        draw(Tile.createCharacterTile(symbol, style), position)
     }
-
-    private fun tileAt(position: Position, style: StyleSet, symbol: Char) =
-            Components.icon()
-                    .withIcon(Tile.createCharacterTile(symbol, style))
-                    .withPosition(position)
 }
