908fa37a738a69a61c44fd19989baa883dafd69f
Loom <Loomie@users.noreply.github.com>
2020-05-01 12:20:27 +0200

    use type and business functions instead of directly accessing attribute

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/systems/facets/MoveTo.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/systems/facets/MoveTo.kt
@@ -2,12 +2,13 @@ package de.gleex.pltcmd.game.engine.systems.facets
 
 import de.gleex.pltcmd.game.engine.GameContext
 import de.gleex.pltcmd.game.engine.attributes.DestinationAttribute
-import de.gleex.pltcmd.game.engine.extensions.getAttribute
+import de.gleex.pltcmd.game.engine.entities.types.Movable
+import de.gleex.pltcmd.game.engine.entities.types.destination
+import de.gleex.pltcmd.game.engine.extensions.GameEntity
 import de.gleex.pltcmd.model.world.coordinate.Coordinate
 import org.hexworks.amethyst.api.Command
 import org.hexworks.amethyst.api.Consumed
 import org.hexworks.amethyst.api.base.BaseFacet
-import org.hexworks.amethyst.api.entity.Entity
 import org.hexworks.amethyst.api.entity.EntityType
 import org.hexworks.cobalt.datatypes.Maybe
 
@@ -15,14 +16,13 @@ import org.hexworks.cobalt.datatypes.Maybe
 data class MoveTo(
         val destination: Coordinate,
         override val context: GameContext,
-        override val source: Entity<EntityType, GameContext>
+        override val source: GameEntity<Movable>
 ) : Command<EntityType, GameContext>
 
 internal object SetDestination : BaseFacet<GameContext>(DestinationAttribute::class) {
     override suspend fun executeCommand(command: Command<out EntityType, GameContext>) =
             command.responseWhenCommandIs(MoveTo::class) { (destination, _, entity) ->
-                entity.getAttribute(DestinationAttribute::class).coordinate = Maybe.of(destination)
+                entity.destination = Maybe.of(destination)
                 Consumed
             }
-
 }
