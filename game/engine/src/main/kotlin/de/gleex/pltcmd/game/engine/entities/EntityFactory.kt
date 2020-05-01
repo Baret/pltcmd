544c9a92eb290376393a06a43304672853099e18
Loom <Loomie@users.noreply.github.com>
2020-05-01 11:12:56 +0200

    directly call method

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/entities/EntityFactory.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/entities/EntityFactory.kt
@@ -1,6 +1,5 @@
 package de.gleex.pltcmd.game.engine.entities
 
-import de.gleex.pltcmd.game.engine.GameContext
 import de.gleex.pltcmd.game.engine.attributes.DestinationAttribute
 import de.gleex.pltcmd.game.engine.attributes.ElementAttribute
 import de.gleex.pltcmd.game.engine.attributes.PositionAttribute
@@ -10,18 +9,15 @@ import de.gleex.pltcmd.game.engine.systems.behaviours.Wandering
 import de.gleex.pltcmd.game.engine.systems.facets.SetDestination
 import de.gleex.pltcmd.model.elements.Element
 import de.gleex.pltcmd.model.world.coordinate.Coordinate
-import org.hexworks.amethyst.api.builder.EntityBuilder
-import org.hexworks.amethyst.api.entity.EntityType
 import org.hexworks.amethyst.api.newEntityOfType
 
 object EntityFactory {
-    private fun <T : EntityType> newEntity(type: T, init: EntityBuilder<T, GameContext>.() -> Unit) =
-            newEntityOfType(type, init)
 
     fun newElement(element: Element, initialPosition: Coordinate): GameEntity<ElementType> =
-            newEntity(ElementType, {
+            newEntityOfType(ElementType, {
                 attributes(ElementAttribute(element), PositionAttribute(initialPosition), DestinationAttribute())
                 behaviors(Moving, Wandering)
                 facets(SetDestination)
             })
+
 }
