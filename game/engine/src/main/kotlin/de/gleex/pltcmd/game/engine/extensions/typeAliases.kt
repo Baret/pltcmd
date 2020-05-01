530faf85149d3168581e1507e0c371944fdc927e
Loom <Loomie@users.noreply.github.com>
2020-05-01 12:23:45 +0200

    improved KDoc

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/extensions/typeAliases.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/extensions/typeAliases.kt
@@ -4,11 +4,11 @@ import de.gleex.pltcmd.game.engine.GameContext
 import org.hexworks.amethyst.api.entity.Entity
 
 /**
- * A specific entity type using the [GameContext]
+ * Entity with a specific type using the [GameContext]
  */
 typealias GameEntity<T> = Entity<T, GameContext>
 
 /**
- * Unspecified entity type using the [GameContext]
+ * Entity with unspecified type using the [GameContext]
  */
 typealias AnyGameEntity = GameEntity<*>
