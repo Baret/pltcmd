d0149992ddbc69ccc9b06df140716592b81e48d4
Loom <Loomie@users.noreply.github.com>
2020-05-01 11:10:21 +0200

    removed hidden complexity

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/DestinationAttribute.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/DestinationAttribute.kt
@@ -2,16 +2,9 @@ package de.gleex.pltcmd.game.engine.attributes
 
 import de.gleex.pltcmd.model.world.coordinate.Coordinate
 import org.hexworks.amethyst.api.Attribute
-import org.hexworks.cobalt.databinding.api.extension.createPropertyFrom
 import org.hexworks.cobalt.datatypes.Maybe
 
 /** The destination of an entity on the map. */
 class DestinationAttribute(initialDestination: Coordinate? = null) : Attribute {
-    private val coordinateProperty = createPropertyFrom(Maybe.ofNullable(initialDestination))
-
-    var coordinate: Maybe<Coordinate>
-        get() = coordinateProperty.value
-        set(value) {
-            coordinateProperty.updateValue(value)
-        }
+    var coordinate = Maybe.ofNullable(initialDestination)
 }
