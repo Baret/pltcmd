ce9a4c1a7a8f01e29b70f9314aa2bfa31cd54c2a
Loom <Loomie@users.noreply.github.com>
2020-05-01 12:16:25 +0200

    removed unused function

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/extensions/EntityExtensions.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/extensions/EntityExtensions.kt
@@ -8,14 +8,6 @@ import kotlin.reflect.KClass
  * Gets the [Attribute] of given type or throws an [IllegalStateException] if no attribute of this type is present.
  *
  * Be sure the attribute exists when using.
- *
- * @see hasAttribute
  */
 internal fun <T : Attribute> AnyGameEntity.getAttribute(attribute: KClass<T>) =
         findAttribute(attribute).orElseThrow { IllegalStateException("Entity $this does not have an attribute of type $attribute") }
-
-/**
- * Returns true if this entity has an [Attribute] of given class.
- */
-internal fun <T : Attribute> AnyGameEntity.hasAttribute(attribute: KClass<T>): Boolean =
-        findAttribute(attribute).isPresent
