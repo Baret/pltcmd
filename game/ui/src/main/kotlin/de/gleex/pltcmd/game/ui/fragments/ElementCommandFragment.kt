140b31f7bd1181b12c21a1225c148bf60c5c2f71
Baret <beret14@web.de>
2020-05-04 22:32:03 +0200

    removed logging from ElementCommandFragment.kt

--- game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/fragments/ElementCommandFragment.kt
+++ game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/fragments/ElementCommandFragment.kt
@@ -11,7 +11,6 @@ import de.gleex.pltcmd.model.radio.communication.Conversations
 import org.hexworks.cobalt.databinding.api.binding.bindPlusWith
 import org.hexworks.cobalt.databinding.api.extension.createPropertyFrom
 import org.hexworks.cobalt.databinding.internal.binding.ComputedBinding
-import org.hexworks.cobalt.logging.api.LoggerFactory
 import org.hexworks.zircon.api.Components
 import org.hexworks.zircon.api.Fragments
 import org.hexworks.zircon.api.data.Position
@@ -33,8 +32,6 @@ class ElementCommandFragment(override val width: Int, private val world: GameWor
                                     withToStringMethod { it.callsign.toString() }.
                                     build()
 
-    private val log = LoggerFactory.getLogger(ElementCommandFragment::class)
-
     override val root = Components.vbox().
             withSize(width, 3).
             build().
@@ -54,7 +51,6 @@ class ElementCommandFragment(override val width: Int, private val world: GameWor
                     build().
                     apply {
                         onActivated {
-                            log.debug("Button clicked! Got event $it")
                             Ticker.sendCommand(
                                 selectedElement,
                                 Conversations.Orders.moveTo(CallSign("HQ"), selectedElement.callsign, destinationProperty.value),
@@ -65,7 +61,6 @@ class ElementCommandFragment(override val width: Int, private val world: GameWor
 
     override fun invoke(event: MouseEvent, phase: UIEventPhase): UIEventResponse {
         val coord = world.coordinateAtVisiblePosition(event.position.minus(mapOffset))
-        log.debug("Coordinate at position ${event.position} is $coord")
         destinationProperty.updateValue(coord)
         return UIEventResponse.processed()
     }
