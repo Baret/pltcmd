e8812ca55e71c974a2b6b361a164293ad25e51f7
Loom <Loomie@users.noreply.github.com>
2020-05-05 19:57:48 +0200

    adding soldier to element (commander), renamed variables

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/Game.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/Game.kt
@@ -3,9 +3,7 @@ package de.gleex.pltcmd.game.engine
 import de.gleex.pltcmd.game.engine.entities.EntityFactory
 import de.gleex.pltcmd.game.engine.extensions.ElementEntity
 import de.gleex.pltcmd.game.engine.extensions.GameEntity
-import de.gleex.pltcmd.model.elements.Affiliation
-import de.gleex.pltcmd.model.elements.CallSign
-import de.gleex.pltcmd.model.elements.Element
+import de.gleex.pltcmd.model.elements.*
 import de.gleex.pltcmd.model.world.Sector
 import de.gleex.pltcmd.model.world.WorldMap
 import org.hexworks.amethyst.api.Engine
@@ -35,14 +33,14 @@ data class Game(val engine: Engine<GameContext>, val world: WorldMap, val random
     fun addElementInSector(sector: Sector, callsign: String = "Element ${random.nextInt(999_999)}", affiliation: Affiliation = Affiliation.Unknown): ElementEntity {
         val positionInSector = sector.randomCoordinate(random)
         val callSign = CallSign(callsign)
-        val elementAttribute = Element(callSign, emptySet())
-        val element = if(affiliation == Affiliation.Friendly) {
-                EntityFactory.newElement(elementAttribute, positionInSector, affiliation)
+        val element = Element(callSign, setOf(GenericUnit(UnitType.Soldier)))
+        val elementEntity = if(affiliation == Affiliation.Friendly) {
+                EntityFactory.newElement(element, positionInSector, affiliation)
             } else {
-                EntityFactory.newWanderingElement(elementAttribute, positionInSector, affiliation)
+                EntityFactory.newWanderingElement(element, positionInSector, affiliation)
             }
         log.debug("Adding element with callsign $callSign to engine at position $positionInSector")
-        return addEntity(element)
+        return addEntity(elementEntity)
     }
 
 }
