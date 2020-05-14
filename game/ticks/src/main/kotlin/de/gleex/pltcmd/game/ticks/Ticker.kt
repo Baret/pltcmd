ade36a831ca464877bb6e148f88d2bf40875ef82
Baret <Beret14@web.de>
2020-05-14 17:10:54 +0200

    make game restartable

--- game/ticks/src/main/kotlin/de/gleex/pltcmd/game/ticks/Ticker.kt
+++ game/ticks/src/main/kotlin/de/gleex/pltcmd/game/ticks/Ticker.kt
@@ -42,7 +42,7 @@ object Ticker {
 
     private val executor = Executors.newScheduledThreadPool(1)
 
-    private lateinit var currentGame: Game
+    private var currentGame: Game? = null
 
     init {
         val initialTime = LocalTime.of(5, 59)
@@ -78,7 +78,7 @@ object Ticker {
 
     fun start(game: Game) {
         currentGame = game
-        with(currentGame) {
+        with(currentGame!!) {
             executor.scheduleAtFixedRate({
                 tick()
                 engine.update(context(currentTick.value))
@@ -88,14 +88,18 @@ object Ticker {
 
     fun stopGame() {
         executor.shutdown()
+        currentGame = null
     }
 
     fun sendCommand(targetEntity: ElementEntity, conversation: Conversation, destination: Coordinate) {
         // TODO: The target would be the "radio net", for now we directly send a move to command to an element
+        require(currentGame != null) {
+            "Currently there is no game running"
+        }
         runBlocking {
             // TODO: The source of the event would be "HQ" which the player controls
             log.debug("Sending conversation to ${targetEntity.callsign}: $conversation")
-            targetEntity.sendCommand(MoveTo(destination, currentGame.context(nextTick.value), targetEntity))
+            targetEntity.sendCommand(MoveTo(destination, currentGame!!.context(nextTick.value), targetEntity))
         }
     }
 }
