6a763dbacff6d99eec07feb0a43751ac95e48f2f
Loom <Loomie@users.noreply.github.com>
2020-06-21 11:16:13 +0200

    stats for combat: Health and fire power

---
+++ model/elements/src/main/kotlin/de/gleex/pltcmd/model/elements/combat/CombatStats.kt
@@ -0,0 +1,21 @@
+package de.gleex.pltcmd.model.elements.combat
+
+import org.hexworks.cobalt.databinding.api.binding.bindTransform
+import org.hexworks.cobalt.databinding.api.extension.toProperty
+import org.hexworks.cobalt.databinding.api.property.Property
+import org.hexworks.cobalt.databinding.api.value.ObservableValue
+
+/**
+ * All values needed for combat.
+ * @param firepower the amount of damage done per tick
+ * @param health the current amount of health left
+ */
+data class CombatStats(val firepower: ObservableValue<Int> = 20.toProperty(), val health: Property<Int> = 100.toProperty()) {
+    /** return false if the health reaches 0 */
+    val isAlive: ObservableValue<Boolean> = health.bindTransform { it > 0 }
+
+    fun attack(target: CombatStats) {
+        target.health.transformValue { it - firepower.value }
+    }
+
+}
