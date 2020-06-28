b915a2fe0cd858d56f2ddbea5f2ea00500d5947c
Loom <Loomie@users.noreply.github.com>
2020-06-28 09:27:39 +0200

    facet for executing commands reacts on all Orders

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/systems/facets/OrderCommand.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/systems/facets/OrderCommand.kt
@@ -5,13 +5,15 @@ import de.gleex.pltcmd.game.engine.attributes.ElementAttribute
 import de.gleex.pltcmd.game.engine.entities.types.ElementEntity
 import de.gleex.pltcmd.game.engine.entities.types.ElementType
 import de.gleex.pltcmd.model.radio.communication.Conversations
+import de.gleex.pltcmd.model.radio.communication.Conversations.Orders.*
+import de.gleex.pltcmd.model.radio.communication.Conversations.Orders.MoveTo
 import de.gleex.pltcmd.model.world.coordinate.Coordinate
 import kotlinx.coroutines.runBlocking
 import org.hexworks.amethyst.api.Command
-import org.hexworks.amethyst.api.Consumed
 import org.hexworks.amethyst.api.base.BaseFacet
 import org.hexworks.amethyst.api.entity.EntityType
 import org.hexworks.cobalt.logging.api.LoggerFactory
+import de.gleex.pltcmd.game.engine.systems.facets.MoveTo as MoveToCommand
 
 /** Command to give an element an order **/
 data class OrderCommand(
@@ -26,14 +28,13 @@ internal object ExecuteOrder : BaseFacet<GameContext>(ElementAttribute::class) {
 
     override suspend fun executeCommand(command: Command<out EntityType, GameContext>) =
             command.responseWhenCommandIs(OrderCommand::class) { (order, orderedTo, context, entity) ->
-                when (order) {
-                    Conversations.Orders.MoveTo -> runBlocking {
-                        entity.executeCommand(MoveTo(orderedTo!!, context, entity))
-                    }
-                    else                        -> {
-                        // TODO implement all orders and remove `else`
-                        log.warn("Don't know how to execute order $order ¯\\_(ツ)_/¯")
-                        Consumed
+                runBlocking {
+                    when (order) {
+                        MoveTo        -> entity.executeCommand(MoveToCommand(orderedTo!!, context, entity))
+                        // TODO just moving to the enemy might not be the best approach
+                        EngageEnemyAt -> entity.executeCommand(MoveToCommand(orderedTo!!, context, entity))
+                        // TODO implement go firm
+                        GoFirm        -> TODO()
                     }
                 }
             }
