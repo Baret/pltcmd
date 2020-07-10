4c788482d0eefdb4b17db3f83056c491e878f76b
Baret <beret14@web.de>
2020-07-10 21:16:37 +0200

    use beInstanceOf<>

--- model/elements/src/test/kotlin/de/gleex/pltcmd/model/elements/blueprint/CommandingElementBlueprintTest.kt
+++ model/elements/src/test/kotlin/de/gleex/pltcmd/model/elements/blueprint/CommandingElementBlueprintTest.kt
@@ -9,24 +9,22 @@ import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.inspectors.forAll
 import io.kotest.inspectors.forNone
 import io.kotest.matchers.beInstanceOf
-import io.kotest.matchers.instanceOf
 import io.kotest.matchers.should
-import io.kotest.matchers.shouldBe
 
 class CommandingElementBlueprintTest : ShouldSpec() {
     init {
         context("The default element blueprint for a rifle platoon should contain the correct sub-blueprints and no actual elements/units.") {
             val platoonBlueprint = Elements.riflePlatoon
-            platoonBlueprint should beInstanceOf(CommandingElementBlueprint::class)
+            platoonBlueprint should beInstanceOf<CommandingElementBlueprint>()
             assertUnits(platoonBlueprint)
             context("Platoon") {
                 should("should only contain ${CommandingElementBlueprint::class.simpleName}s") {
                     platoonBlueprint.subordinates.forAll {
-                        it should beInstanceOf(CommandingElementBlueprint::class)
+                        it should beInstanceOf<CommandingElementBlueprint>()
                     }
                     platoonBlueprint.subordinates.forNone {
-                        it shouldBe instanceOf(Element::class)
-                        it shouldBe instanceOf(ElementBlueprint::class)
+                        it should beInstanceOf<Element>()
+                        it should beInstanceOf<ElementBlueprint>()
                     }
                 }
             }
@@ -39,7 +37,7 @@ class CommandingElementBlueprintTest : ShouldSpec() {
                             assertUnits(squad)
                             should("should only contain ${ElementBlueprint::class.simpleName}s") {
                                 squad.subordinates.forAll {
-                                    it should beInstanceOf(ElementBlueprint::class)
+                                    it should beInstanceOf<ElementBlueprint>()
                                 }
                                 squad.subordinates.forNone {
                                     it should beInstanceOf<Element>()
@@ -52,10 +50,10 @@ class CommandingElementBlueprintTest : ShouldSpec() {
                                         context("Fireteam ($i)") {
                                             should("should only have unit blueprints, no units") {
                                                 (fireteam as ElementBlueprint).units.forAll {
-                                                    it should beInstanceOf(Units::class)
+                                                    it should beInstanceOf<Units>()
                                                 }
                                                 fireteam.units.forNone {
-                                                    it should beInstanceOf(Unit::class)
+                                                    it should beInstanceOf<Unit>()
                                                 }
                                             }
                                         }
@@ -68,10 +66,10 @@ class CommandingElementBlueprintTest : ShouldSpec() {
     private suspend fun ContextScope.assertUnits(commandingElementBlueprint: CommandingElementBlueprint) {
         should("only contain unit blueprints, no units") {
             commandingElementBlueprint.units.forAll {
-                it should beInstanceOf(Units::class)
+                it should beInstanceOf<Units>()
             }
             commandingElementBlueprint.units.forNone {
-                it should beInstanceOf(Unit::class)
+                it should beInstanceOf<Unit>()
             }
         }
     }
