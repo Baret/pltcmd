8c9f5d1a3c2db47bedcaf271b649763b78adff19
Baret <beret14@web.de>
2020-07-16 17:56:18 +0200

    simplified ElementTest

--- model/elements/src/test/kotlin/de/gleex/pltcmd/model/elements/ElementTest.kt
+++ model/elements/src/test/kotlin/de/gleex/pltcmd/model/elements/ElementTest.kt
@@ -4,12 +4,10 @@ import de.gleex.pltcmd.model.elements.units.UnitKind
 import de.gleex.pltcmd.model.elements.units.Units
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
-import io.kotest.inspectors.forExactly
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.collections.shouldNotContain
-import io.kotest.matchers.shouldBe
 
 class ElementTest : WordSpec({
 
@@ -18,9 +16,6 @@ class ElementTest : WordSpec({
     val simpleElement = Element(Corps.Reconnaissance, ElementKind.MotorizedInfantry, Rung.Fireteam, setOf(officer, rifleman))
     "A simple element $simpleElement" should {
         "contain 2 units" {
-            simpleElement.units shouldHaveSize 2
-            simpleElement.units.forExactly(1) { it.blueprint shouldBe Units.Officer }
-            simpleElement.units.forExactly(1) { it.blueprint shouldBe Units.Rifleman }
             simpleElement.units shouldContainExactlyInAnyOrder setOf(officer, rifleman)
         }
         val newUnit = Units.RadioJeep.new()
