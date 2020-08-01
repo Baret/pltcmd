ebd75d51fa03760d3b6b11836eaf178188cf4a4c
Baret <beret14@web.de>
2020-08-01 23:21:44 +0200

    made GoalTest more verbose

--- game/engine/src/test/kotlin/de/gleex/pltcmd/game/engine/attributes/goals/GoalTest.kt
+++ game/engine/src/test/kotlin/de/gleex/pltcmd/game/engine/attributes/goals/GoalTest.kt
@@ -6,7 +6,7 @@ import de.gleex.pltcmd.game.engine.entities.types.ElementType
 import de.gleex.pltcmd.game.ticks.TickId
 import de.gleex.pltcmd.model.world.WorldMap
 import de.gleex.pltcmd.util.tests.beEmptyMaybe
-import io.kotest.core.spec.style.WordSpec
+import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.beInstanceOf
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
@@ -18,7 +18,7 @@ import org.hexworks.amethyst.api.newEntityOfType
 import org.hexworks.cobalt.datatypes.Maybe
 import kotlin.random.Random
 
-class GoalTest : WordSpec() {
+class GoalTest : FreeSpec() {
 
     private val testEntity: ElementEntity = newEntityOfType(ElementType) {
         // no attributes etc. needed yet
@@ -33,19 +33,25 @@ class GoalTest : WordSpec() {
 
     init {
 
-        "Sub-goals" should {
-            "be stepped in the correct order by the default implementation" {
-                val testGoal = TestGoal(TestSubGoal(1), TestSubGoal(2), TestSubGoal(3))
+        "A goal containing 3 sub-goals should" - {
+            val testGoal = TestGoal(TestSubGoal(1), TestSubGoal(2), TestSubGoal(3))
+            "step through them in the correct order:" - {
 
                 var expectedValue = 1
                 repeat(3) {
-                    testGoal.isFinished(testEntity) shouldBe false
-                    val commandMaybe = testGoal.step(testEntity, testContext)
-                    commandMaybe shouldNot beEmptyMaybe()
-                    val command = commandMaybe.get()
-                    command should beInstanceOf<TestCommand>()
-                    (command as TestCommand).value shouldBe expectedValue
-                    expectedValue++
+                    "Iteration $it:" - {
+                        "The goal should not yet be finished" {
+                            testGoal.isFinished(testEntity) shouldBe false
+                        }
+                        val commandMaybe = testGoal.step(testEntity, testContext)
+                        "The returned command should contain value $expectedValue" {
+                            commandMaybe shouldNot beEmptyMaybe()
+                            val command = commandMaybe.get()
+                            command should beInstanceOf<TestCommand>()
+                            (command as TestCommand).value shouldBe expectedValue
+                        }
+                        expectedValue++
+                    }
                 }
 
                 testGoal.step(testEntity, testContext) should beEmptyMaybe()
@@ -54,8 +60,17 @@ class GoalTest : WordSpec() {
         }
     }
 
+    /**
+     * Simply inherits from [Goal] and uses the default implementations.
+     */
     private class TestGoal(vararg subGoals: Goal) : Goal(*subGoals)
 
+    /**
+     * A goal used as sub-goal that is finished after one invocation of [step]. This invocation returns a [TestCommand]
+     * containing [value].
+     *
+     * @param value to be put into the [TestCommand] returned by [step].
+     */
     private data class TestSubGoal(val value: Int) : Goal() {
         private var finished = false
         override fun isFinished(element: ElementEntity) = finished
