be4518e42d7f02638e0026b6f72e128e8bd60a59
Baret <beret14@web.de>
2020-08-01 23:04:02 +0200

    created test for default step implementation

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/goals/Goal.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/goals/Goal.kt
@@ -4,6 +4,7 @@ import de.gleex.pltcmd.game.engine.GameContext
 import de.gleex.pltcmd.game.engine.entities.types.ElementEntity
 import org.hexworks.amethyst.api.Command
 import org.hexworks.cobalt.datatypes.Maybe
+import org.hexworks.cobalt.logging.api.LoggerFactory
 import java.util.*
 
 /**
@@ -16,8 +17,12 @@ abstract class Goal(vararg subGoals: Goal) {
 
     private val subGoals = Stack<Goal>()
 
+    private val log = LoggerFactory.getLogger(Goal::class)
+
     init {
+        log.debug("Initializing goal with ${subGoals.size} subgoals")
         subGoals.reversed().forEach {
+            log.debug("\tPushing subgoal $it")
             this.subGoals.push(it)
         }
     }
@@ -25,23 +30,32 @@ abstract class Goal(vararg subGoals: Goal) {
     /**
      * Returns true if this goal does not need any more steps.
      */
-    abstract fun isFinished(element: ElementEntity): Boolean
+    open fun isFinished(element: ElementEntity): Boolean =
+            subGoals.isEmpty()
 
     /**
      * Advances this goal by one step. It may return a command that needs to be executed by the element.
      *
      * It should not call execute/sendCommand directly. At most this method might set attributes.
+     *
+     * The default implementation simply advances the sub-goals by calling [stepSubGoals].
      */
-    abstract fun step(element: ElementEntity, context: GameContext): Maybe<Command<*, GameContext>>
+    open fun step(element: ElementEntity, context: GameContext): Maybe<Command<*, GameContext>> =
+            stepSubGoals(element, context)
 
     /**
      * Calls [step] on the next unfinished sub-goal. All finished goals are popped off the sub-goal-stack.
      */
     protected fun stepSubGoals(element: ElementEntity, context: GameContext): Maybe<Command<*, GameContext>> {
-        while (subGoals.peek().isFinished(element)) {
+        while (subGoals.isNotEmpty() && subGoals.peek().isFinished(element)) {
             subGoals.pop()
         }
-        return subGoals.peek().step(element, context)
+        return if(subGoals.isEmpty()) {
+            Maybe.empty()
+        } else {
+            subGoals.peek()
+                    .step(element, context)
+        }
     }
 }
 
---
+++ game/engine/src/test/kotlin/de/gleex/pltcmd/game/engine/attributes/goals/GoalTest.kt
@@ -0,0 +1,78 @@
+package de.gleex.pltcmd.game.engine.attributes.goals
+
+import de.gleex.pltcmd.game.engine.GameContext
+import de.gleex.pltcmd.game.engine.entities.types.ElementEntity
+import de.gleex.pltcmd.game.engine.entities.types.ElementType
+import de.gleex.pltcmd.game.ticks.TickId
+import de.gleex.pltcmd.model.world.WorldMap
+import de.gleex.pltcmd.util.tests.beEmptyMaybe
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.beInstanceOf
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNot
+import io.mockk.mockkClass
+import org.hexworks.amethyst.api.Command
+import org.hexworks.amethyst.api.entity.Entity
+import org.hexworks.amethyst.api.newEntityOfType
+import org.hexworks.cobalt.datatypes.Maybe
+import org.hexworks.cobalt.logging.api.LoggerFactory
+import kotlin.random.Random
+
+class GoalTest : WordSpec() {
+
+    private val log = LoggerFactory.getLogger(GoalTest::class)
+
+    private val testEntity: ElementEntity = newEntityOfType(ElementType) {
+        // no attributes etc. needed yet
+    }
+
+    private val testContext: GameContext = GameContext(
+            currentTick = TickId(12),
+            world = mockkClass(WorldMap::class),
+            allElements = setOf(),
+            random = Random
+    )
+
+    init {
+
+        "Sub-goals" should {
+            "be stepped in the correct order by the default implementation" {
+                val testGoal = TestGoal(TestSubGoal(1), TestSubGoal(2), TestSubGoal(3))
+
+                var expectedValue = 1
+                repeat(3) {
+                    log.debug("testing $testGoal with expected value $expectedValue...")
+                    testGoal.isFinished(testEntity) shouldBe false
+                    val commandMaybe = testGoal.step(testEntity, testContext)
+                    commandMaybe shouldNot beEmptyMaybe()
+                    val command = commandMaybe.get()
+                    command should beInstanceOf<TestCommand>()
+                    (command as TestCommand).value shouldBe expectedValue
+                    expectedValue++
+                }
+
+                testGoal.step(testEntity, testContext) should beEmptyMaybe()
+                testGoal.isFinished(testEntity) shouldBe true
+            }
+        }
+    }
+
+    private class TestGoal(vararg subGoals: Goal) : Goal(*subGoals)
+
+    private data class TestSubGoal(val value: Int) : Goal() {
+        private var finished = false
+        override fun isFinished(element: ElementEntity) = finished
+
+        override fun step(element: ElementEntity, context: GameContext): Maybe<Command<*, GameContext>> {
+            finished = true
+            return Maybe.of(TestCommand(value, element, context))
+        }
+    }
+
+    private data class TestCommand(
+            val value: Int,
+            override val source: Entity<ElementType, GameContext>,
+            override val context: GameContext
+    ) : Command<ElementType, GameContext>
+}
