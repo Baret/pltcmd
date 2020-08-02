b9dba7831397242c09e900ad8a2362ecd2eff76c
Baret <beret14@web.de>
2020-08-02 23:16:35 +0200

    phew, fixed it

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/goals/RootGoal.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/goals/RootGoal.kt
@@ -1,19 +1,12 @@
 package de.gleex.pltcmd.game.engine.attributes.goals
 
-import de.gleex.pltcmd.game.engine.GameContext
 import de.gleex.pltcmd.game.engine.attributes.CommandersIntent
-import de.gleex.pltcmd.game.engine.entities.types.ElementEntity
-import org.hexworks.amethyst.api.Command
 import org.hexworks.cobalt.datatypes.Maybe
 
 /**
  * The root goal is directly and only used by the [CommandersIntent] and is the only goal exposing the sub-goal API.
  */
 class RootGoal : EndlessGoal() {
-    override fun step(element: ElementEntity, context: GameContext): Maybe<Command<*, GameContext>> {
-        return Maybe.empty()
-    }
-
     fun clear() =
             this.also {
                 super.clearSubGoals()
