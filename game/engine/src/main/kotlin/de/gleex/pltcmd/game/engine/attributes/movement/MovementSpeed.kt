003ebe43ad6c2e3d7e37036f6da3d40029039e1d
Baret <beret14@web.de>
2020-08-04 13:37:59 +0200

    Documented and improved ElementStats

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/movement/MovementSpeed.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/movement/MovementSpeed.kt
@@ -6,21 +6,23 @@ import de.gleex.pltcmd.model.elements.units.Unit
 import de.gleex.pltcmd.model.elements.units.UnitKind
 import de.gleex.pltcmd.model.elements.units.Units
 
+/**
+ * The base speed of an element depending on the speed of each [Unit].
+ */
 class MovementSpeed(element: CommandingElement) : ElementStats(element) {
-    override val unitStats: Map<Unit, Double> =
-            element.allUnits.associateWith {
-                when(it.kind) {
-                    UnitKind.Infantry -> 3.5
-                    UnitKind.Unarmored -> 15.0
-                    UnitKind.ArmoredLight -> 20.0
-                    UnitKind.ArmoredHeavy -> 17.0
-                    UnitKind.AerialLight ->
-                        when(it.blueprint) {
-                            Units.ScoutPlane -> 160.0
-                            else             -> 120.0
-                        }
-                    UnitKind.AerialHeavy -> 90.0
-                }
+    override fun valueForUnit(unit: Unit): Double =
+            // TODO: Find reasonable values and maybe determine them more fine grained
+            when (unit.kind) {
+                UnitKind.Infantry     -> 3.5
+                UnitKind.Unarmored    -> 15.0
+                UnitKind.ArmoredLight -> 20.0
+                UnitKind.ArmoredHeavy -> 17.0
+                UnitKind.AerialLight  ->
+                    when (unit.blueprint) {
+                        Units.ScoutPlane -> 160.0
+                        else             -> 120.0
+                    }
+                UnitKind.AerialHeavy  -> 90.0
             }
 
     override val value: Double
--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/stats/ElementStats.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/stats/ElementStats.kt
@@ -4,8 +4,26 @@ import de.gleex.pltcmd.model.elements.CommandingElement
 import de.gleex.pltcmd.model.elements.units.Unit
 import org.hexworks.amethyst.api.Attribute
 
+/**
+ * Stats for an element. As an element consists of several [Unit]s implementations of this class
+ * map a value to each unit. It can then use the average, minimum or maximum value from that mapping as "the value
+ * for the whole element" (which is [value]).
+ */
 abstract class ElementStats(element: CommandingElement) : Attribute {
-    protected abstract val unitStats: Map<Unit, Double>
+    /**
+     * The internal map that holds a value for each unit of the element.
+     *
+     * Use [average], [minimum] or [maximum] for easy access to accumulated values that might be used as [value].
+     */
+    protected val unitStats: Map<Unit, Double>  =
+            element.allUnits.associateWith {
+                valueForUnit(it)
+            }
+
+    /**
+     * Returns the value for the given unit. This method is used to initialize [unitStats]
+     */
+    protected abstract fun valueForUnit(unit: Unit): Double
 
     protected val average: Double
         get() = unitStats.values.average()
@@ -14,10 +32,13 @@ abstract class ElementStats(element: CommandingElement) : Attribute {
     protected val maximum: Double
         get() = unitStats.values.max() ?: 0.0
 
+    /**
+     * The value for the whole element calculated from the values stored for all units.
+     */
     abstract val value: Double
 
     /**
-     * @return the value for the given [Unit] or null if the unit is nor present.
+     * @return the value for the given [Unit] or null if the unit is not present.
      */
     operator fun get(unit: Unit): Double? = unitStats[unit]
 }
