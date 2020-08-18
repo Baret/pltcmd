b4d7af53da61fce8a8ac5c14915448d51f2c17a9
Baret <beret14@web.de>
2020-08-18 14:02:48 +0200

    rudimentary mouse handling in InfoSidebar

--- game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/components/InfoSidebar.kt
+++ game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/components/InfoSidebar.kt
@@ -3,11 +3,16 @@ package de.gleex.pltcmd.game.ui.components
 import de.gleex.pltcmd.game.options.UiOptions
 import de.gleex.pltcmd.game.ui.entities.GameWorld
 import de.gleex.pltcmd.game.ui.fragments.CoordinateAtMousePosition
+import org.hexworks.cobalt.logging.api.LoggerFactory
 import org.hexworks.zircon.api.ComponentDecorations
 import org.hexworks.zircon.api.Components
 import org.hexworks.zircon.api.component.Fragment
 import org.hexworks.zircon.api.game.GameComponent
 import org.hexworks.zircon.api.graphics.BoxType
+import org.hexworks.zircon.api.uievent.MouseEventType
+import org.hexworks.zircon.api.uievent.Pass
+import org.hexworks.zircon.api.uievent.Processed
+import org.hexworks.zircon.api.uievent.UIEventPhase
 
 /**
  * The info sidebar displays contextual information for the player like "what is on that tile?".
@@ -19,7 +24,18 @@ class InfoSidebar(height: Int, map: GameComponent<*, *>, gameWorld: GameWorld) :
                     .withDecorations(ComponentDecorations.box(BoxType.DOUBLE, "Intel"))
                     .build()
 
+    private val log = LoggerFactory.getLogger(InfoSidebar::class)
+
     init {
         root.addFragment(CoordinateAtMousePosition(root.contentSize.width, map, gameWorld))
+
+        map.handleMouseEvents(MouseEventType.MOUSE_CLICKED) {event, phase ->
+            if(phase == UIEventPhase.TARGET && event.button == 2) {
+                log.debug("RIGHT MOUSE BUTTON CLICKED!")
+                Processed
+            } else {
+                Pass
+            }
+        }
     }
 }
