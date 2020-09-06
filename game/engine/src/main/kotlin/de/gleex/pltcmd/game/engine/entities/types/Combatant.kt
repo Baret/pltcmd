99343775194120bc34c66594f07ef41928fc3c27
Loom <Loomie@users.noreply.github.com>
2020-09-06 13:35:53 +0200

    engine provides time of attack. Partial shots are remembered

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/entities/types/Combatant.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/entities/types/Combatant.kt
@@ -3,9 +3,13 @@ package de.gleex.pltcmd.game.engine.entities.types
 import de.gleex.pltcmd.game.engine.attributes.CombatAttribute
 import de.gleex.pltcmd.game.engine.extensions.GameEntity
 import de.gleex.pltcmd.game.engine.extensions.getAttribute
+import de.gleex.pltcmd.game.options.GameConstants.Time.secondsSimulatedPerTick
 import de.gleex.pltcmd.model.elements.combat.CombatStats
 import org.hexworks.amethyst.api.entity.EntityType
 import kotlin.random.Random
+import kotlin.time.DurationUnit
+import kotlin.time.ExperimentalTime
+import kotlin.time.toDuration
 
 /**
  * This file contains code for entities that have the [CombatAttribute].
@@ -19,6 +23,8 @@ val CombatantEntity.combatStats: CombatStats
     get() = getAttribute(CombatAttribute::class).stats
 
 /** This combatant attacks the given [target] */
+@OptIn(ExperimentalTime::class)
 internal fun CombatantEntity.attack(target: CombatantEntity, random: Random) {
-    combatStats.attack(target.combatStats, random)
+    val attackDurationPerTick = secondsSimulatedPerTick.toDuration(DurationUnit.SECONDS)
+    combatStats.attack(target.combatStats, attackDurationPerTick, random)
 }
--- model/elements/src/main/kotlin/de/gleex/pltcmd/model/elements/combat/CombatStats.kt
+++ model/elements/src/main/kotlin/de/gleex/pltcmd/model/elements/combat/CombatStats.kt
@@ -1,19 +1,30 @@
 package de.gleex.pltcmd.model.elements.combat
 
 import org.hexworks.cobalt.databinding.api.binding.bindTransform
+import org.hexworks.cobalt.databinding.api.collection.ObservableList
 import org.hexworks.cobalt.databinding.api.extension.toProperty
 import org.hexworks.cobalt.databinding.api.property.Property
 import org.hexworks.cobalt.databinding.api.value.ObservableValue
+import org.hexworks.cobalt.databinding.internal.binding.ListBinding
 import org.hexworks.cobalt.logging.api.LoggerFactory
 import kotlin.math.min
 import kotlin.random.Random
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 
 /**
  * All values needed for combat.
  * @param weapons the weapons to attack enemies
  * @param health the current amount of health left
  */
-data class CombatStats(val weapons: ObservableValue<List<Weapon>>, val health: Property<Int> = 100.toProperty()) {
+class CombatStats(weapons: ObservableList<Weapon>, val health: Property<Int> = 100.toProperty()) {
+
+    companion object {
+        internal val log = LoggerFactory.getLogger(CombatStats::class)
+    }
+
+    private val shooters: ListBinding<Weapon, Shooter> = ListBinding(weapons) { Shooter(it) }
+
     /** return false if the health reaches 0 */
     val alive: ObservableValue<Boolean> = health.bindTransform { it > 0 }
 
@@ -21,18 +32,15 @@ data class CombatStats(val weapons: ObservableValue<List<Weapon>>, val health: P
     val isAlive: Boolean
         get() = alive.value
 
-    fun attack(target: CombatStats, random: Random) {
+    @ExperimentalTime
+    fun attack(target: CombatStats, attackDuration: Duration, random: Random) {
         if (target.isAlive) {
-            val damagePerTick = weapons.value.map { it.fireShots(random) }
+            val damagePerTick = shooters.value.map { it.fireShots(attackDuration, random) }
                     .sum()
             val receivedDamage = min(damagePerTick, target.health.value)
             target.health.transformValue { it - receivedDamage }
             log.debug("attack with $damagePerTick damage resulted in target health of ${target.health.value}")
         }
-    }
-
-    companion object {
-        private val log = LoggerFactory.getLogger(CombatStats::class)
     }
 
     infix fun onDeath(callback: () -> Unit) {
@@ -42,17 +50,28 @@ data class CombatStats(val weapons: ObservableValue<List<Weapon>>, val health: P
             callback.invoke()
         }
     }
+}
 
-    /** @return damage of hits per tick */
-    private fun Weapon.fireShots(random: Random): Int {
-        val shotsPerTick = roundsPerMinute / 1 // TODO make GameConstants.Time.ticksPerMinute available
+/** A single shooter with a weapon that can shot at a target. Holds the state of partial shots between mutliple calls. */
+internal class Shooter(private val weapon: Weapon) {
+
+    // remember "half shots" for if a shot is done after a longer time period than given in a single call
+    private var partialShot: Double = 0.0
+
+    /** @return damage of all hits in the given time */
+    @ExperimentalTime
+    fun fireShots(attackDuration: Duration, random: Random): Int {
+        val shotsPerDuration = weapon.roundsPerMinute * attackDuration.inMinutes + partialShot
+        // rounding down loses a partial shot that is remember for the next call.
+        partialShot = shotsPerDuration.rem(1)
+        val fullShots: Int = shotsPerDuration.toInt()
         var hits = 0
-        repeat(shotsPerTick) {
-            if (random.nextDouble() <= shotAccuracy) {
+        repeat(fullShots) {
+            if (random.nextDouble() <= weapon.shotAccuracy) {
                 hits = hits.inc()
             }
         }
-        log.trace("firing $shotsPerTick shots with $shotAccuracy each results in $hits hits")
+        CombatStats.log.info("firing $shotsPerDuration shots in $attackDuration with accuracy ${weapon.shotAccuracy} results in $hits hits")
         return hits * 1 // dmg / shot
     }
 }
