8e7ff86a649b93c567939b77fc6963323cc473d5
Loom <Loomie@users.noreply.github.com>
2020-09-13 14:24:32 +0200

    moved shoot logic to Combatant, reduced Shooter to PartialShot

--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/combat/ShootersAttribute.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/attributes/combat/ShootersAttribute.kt
@@ -3,43 +3,13 @@ package de.gleex.pltcmd.game.engine.attributes.combat
 import de.gleex.pltcmd.model.elements.Element
 import de.gleex.pltcmd.model.elements.combat.Weapon
 import org.hexworks.amethyst.api.Attribute
-import org.hexworks.cobalt.logging.api.LoggerFactory
-import kotlin.random.Random
-import kotlin.time.Duration
-import kotlin.time.ExperimentalTime
 
 /** The offensive part of the given element. */
 internal class ShootersAttribute(weapons: Iterable<Weapon>) : Attribute {
     constructor(element: Element) : this(element.allUnits.map { it.blueprint.weapon })
 
-    internal val shooters: List<Shooter> = weapons.map { Shooter(it) }
-}
-
-/** A single shooter with a weapon that can shoot at a target. Holds the state of partial shots between multiple calls. */
-internal class Shooter(private val weapon: Weapon) {
-
-    companion object {
-        private val log = LoggerFactory.getLogger(Shooter::class)
-    }
-
     // remember "half shots" for if a shot is done after a longer time period than given in a single call
-    private var partialShot: Double = 0.0
-
-    /** @return damage of all hits in the given time */
-    @ExperimentalTime
-    fun fireShots(attackDuration: Duration, random: Random): Int {
-        val shotsPerDuration = weapon.roundsPerMinute * attackDuration.inMinutes + partialShot
-        // rounding down loses a partial shot that is remember for the next call.
-        partialShot = shotsPerDuration.rem(1)
-        val fullShots: Int = shotsPerDuration.toInt()
-        var hits = 0
-        repeat(fullShots) {
-            if (random.nextDouble() <= weapon.shotAccuracy) {
-                hits = hits.inc()
-            }
-        }
-        log.info("firing $shotsPerDuration shots in $attackDuration with accuracy ${weapon.shotAccuracy} results in $hits hits")
-        return hits * 1 // dmg / shot TODO depend on weapon https://github.com/Baret/pltcmd/issues/115
-    }
-
+    internal val shooters: Map<Weapon, PartialShot> = weapons.associateWith { PartialShot(0.0) }
 }
+
+class PartialShot(var value: Double)
--- game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/entities/types/Combatant.kt
+++ game/engine/src/main/kotlin/de/gleex/pltcmd/game/engine/entities/types/Combatant.kt
@@ -1,15 +1,18 @@
 package de.gleex.pltcmd.game.engine.entities.types
 
 import de.gleex.pltcmd.game.engine.attributes.combat.HealthAttribute
+import de.gleex.pltcmd.game.engine.attributes.combat.PartialShot
 import de.gleex.pltcmd.game.engine.attributes.combat.ShootersAttribute
 import de.gleex.pltcmd.game.engine.extensions.GameEntity
 import de.gleex.pltcmd.game.engine.extensions.getAttribute
 import de.gleex.pltcmd.game.options.GameConstants.Time.secondsSimulatedPerTick
+import de.gleex.pltcmd.model.elements.combat.Weapon
 import org.hexworks.amethyst.api.entity.EntityType
 import org.hexworks.cobalt.databinding.api.value.ObservableValue
 import org.hexworks.cobalt.logging.api.LoggerFactory
 import kotlin.math.min
 import kotlin.random.Random
+import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.ExperimentalTime
 import kotlin.time.toDuration
@@ -17,6 +20,7 @@ import kotlin.time.toDuration
 /**
  * This file contains code for entities that have the [ShootersAttribute] and [HealthAttribute].
  */
+private val log = LoggerFactory.getLogger("de.gleex.pltcmd.game.engine.entities.types.Combatant")
 
 interface Combatant : EntityType
 typealias CombatantEntity = GameEntity<Combatant>
@@ -33,7 +37,7 @@ val CombatantEntity.isAlive: Boolean
     get() = healthAtt.isAlive
 
 /** current hit points of this entity */
-val CombatantEntity.health : ObservableValue<Int>
+val CombatantEntity.health: ObservableValue<Int>
     get() = healthAtt.health
 
 infix fun CombatantEntity.onDeath(callback: () -> Unit) {
@@ -45,11 +49,31 @@ infix fun CombatantEntity.onDeath(callback: () -> Unit) {
 internal fun CombatantEntity.attack(target: CombatantEntity, random: Random) {
     val attackDurationPerTick = secondsSimulatedPerTick.toDuration(DurationUnit.SECONDS)
     if (target.isAlive) {
-        val damagePerTick = shootersAtt.shooters.map { it.fireShots(attackDurationPerTick, random) }
+        val damagePerTick = shootersAtt.shooters.map { it.key.fireShots(it.value, attackDurationPerTick, random) }
                 .sum()
         val receivedDamage = min(damagePerTick, target.health.value)
         target.healthAtt - receivedDamage
-        val log = LoggerFactory.getLogger(CombatantEntity::attack::class)
         log.debug("attack with $damagePerTick damage resulted in target health of ${target.health.value}")
     }
+}
+
+/**
+ * side effect: Updates the partial shot that is already done after firing.
+ * @return damage of all hits in the given time.
+ **/
+@ExperimentalTime
+fun Weapon.fireShots(partialShot: PartialShot, attackDuration: Duration, random: Random): Int {
+    val shotsPerDuration = roundsPerMinute * attackDuration.inMinutes + partialShot.value
+    // rounding down loses a partial shot that is remember for the next call.
+    val fullShots: Int = shotsPerDuration.toInt()
+    partialShot.value = shotsPerDuration % 1
+
+    var hits = 0
+    repeat(fullShots) {
+        if (random.nextDouble() <= shotAccuracy) {
+            hits = hits.inc()
+        }
+    }
+    log.info("firing $shotsPerDuration shots in $attackDuration with accuracy ${shotAccuracy} results in $hits hits")
+    return hits * 1 // dmg / shot TODO depend on weapon https://github.com/Baret/pltcmd/issues/115
 }
