6e084f7e9f5f2ca46e6768416a2c3f37721759bf
Baret <450955+Baret@users.noreply.github.com>
2020-09-28 10:57:44 +0200

    Toggle locked state of info sidebar with right click

--- game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/components/InfoSidebar.kt
+++ game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/components/InfoSidebar.kt
@@ -6,11 +6,14 @@ import de.gleex.pltcmd.game.ui.fragments.CoordinateAtMousePosition
 import de.gleex.pltcmd.game.ui.fragments.TickFragment
 import de.gleex.pltcmd.game.ui.fragments.tileinformation.TerrainDetailsFragment
 import de.gleex.pltcmd.model.world.coordinate.Coordinate
+import org.hexworks.cobalt.databinding.api.event.ObservableValueChanged
 import org.hexworks.cobalt.databinding.api.extension.toProperty
 import org.hexworks.cobalt.databinding.api.property.Property
 import org.hexworks.cobalt.logging.api.LoggerFactory
 import org.hexworks.zircon.api.ComponentDecorations
 import org.hexworks.zircon.api.Components
+import org.hexworks.zircon.api.builder.component.ColorThemeBuilder
+import org.hexworks.zircon.api.component.ColorTheme
 import org.hexworks.zircon.api.component.Fragment
 import org.hexworks.zircon.api.game.GameComponent
 import org.hexworks.zircon.api.graphics.BoxType
@@ -23,6 +26,11 @@ import org.hexworks.zircon.api.uievent.UIEventPhase
  * The info sidebar displays contextual information for the player like "what is on that tile?".
  */
 class InfoSidebar(height: Int, map: GameComponent<*, *>, gameWorld: GameWorld) : Fragment {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(InfoSidebar::class)
+    }
+
     override val root =
             Components.vbox()
                     .withSize(UiOptions.SIDEBAR_WIDTH, height)
@@ -30,35 +38,65 @@ class InfoSidebar(height: Int, map: GameComponent<*, *>, gameWorld: GameWorld) :
                     .withDecorations(ComponentDecorations.box(BoxType.DOUBLE, "Intel"))
                     .build()
 
-    companion object {
-        private val log = LoggerFactory.getLogger(InfoSidebar::class)
-    }
+    private val themeUnlocked: ColorTheme = UiOptions.THEME
+    private val themeLocked: ColorTheme = ColorThemeBuilder.newBuilder()
+            .withAccentColor(themeUnlocked.accentColor)
+            .withPrimaryForegroundColor(themeUnlocked.primaryForegroundColor.lightenByPercent(.2))
+            .withPrimaryBackgroundColor(themeUnlocked.primaryBackgroundColor.desaturate(.3))
+            .withSecondaryForegroundColor(themeUnlocked.secondaryForegroundColor.lightenByPercent(.2))
+            .withSecondaryBackgroundColor(themeUnlocked.secondaryBackgroundColor.desaturate(.3))
+            .build()
 
     init {
-        val observedTile: Property<Coordinate> = gameWorld.visibleTopLeftCoordinate().toProperty()
         val fragmentWidth = root.contentSize.width
+
+        val observedTile: Property<Coordinate> = gameWorld.visibleTopLeftCoordinate().toProperty()
         val terrainDetails = TerrainDetailsFragment(fragmentWidth, observedTile, gameWorld.worldMap)
         val coordinateFragment = CoordinateAtMousePosition(fragmentWidth, observedTile)
 
-        map.handleMouseEvents(MouseEventType.MOUSE_MOVED) { event, phase ->
+        val lockedState: Property<Boolean> = false.toProperty()
+        lockedState.onChange { valueChanged: ObservableValueChanged<Boolean> ->
+            log.debug("Toggling lock on information sidebar")
+            terrainDetails.toggleLock()
+            coordinateFragment.toggleLock()
+            if(valueChanged.newValue) {
+                root.themeProperty.updateValue(themeLocked)
+            } else {
+                root.themeProperty.updateValue(themeUnlocked)
+            }
+        }
+
+        map.updateObservedTile(observedTile, gameWorld)
+        map.toggleLockedState(lockedState)
+
+        root.addFragment(TickFragment(fragmentWidth))
+        root.addFragment(coordinateFragment)
+        root.addFragment(terrainDetails)
+    }
+
+    /**
+     * Adds a mouse listener to this [GameComponent] that updates [observedTile] when the mouse moved.
+     */
+    private fun GameComponent<*, *>.updateObservedTile(observedTile: Property<Coordinate>, gameWorld: GameWorld) {
+        handleMouseEvents(MouseEventType.MOUSE_MOVED) { event, phase ->
             if(phase == UIEventPhase.TARGET) {
-                observedTile.updateValue(gameWorld.coordinateAtVisiblePosition(event.position - map.absolutePosition))
+                observedTile.updateValue(gameWorld.coordinateAtVisiblePosition(event.position - absolutePosition))
             }
             Processed
         }
-        map.handleMouseEvents(MouseEventType.MOUSE_CLICKED) {event, phase ->
+    }
+
+    /**
+     * Adds a mouse listener to this [GameComponent] that toggles [lockedState] on right click.
+     */
+    private fun GameComponent<*, *>.toggleLockedState(lockedState: Property<Boolean>) {
+        handleMouseEvents(MouseEventType.MOUSE_CLICKED) {event, phase ->
             if(phase == UIEventPhase.TARGET && event.button == 3) {
-                log.debug("Toggling lock on information sidebar")
-                terrainDetails.toggleLock()
-                coordinateFragment.toggleLock()
+                lockedState.updateValue(!lockedState.value)
                 Processed
             } else {
                 Pass
             }
         }
-
-        root.addFragment(TickFragment(fragmentWidth))
-        root.addFragment(coordinateFragment)
-        root.addFragment(terrainDetails)
     }
 }
