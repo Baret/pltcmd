d793fb56d0c3da1dd5fc188ed5d26c5d8caa43c2
Loomie <Loomie@users.noreply.github.com>
2020-12-21 16:13:51 +0100

    ConcurrentSkipListSet is faster than TreeSet

--- model/world/src/main/kotlin/de/gleex/pltcmd/model/world/coordinate/CoordinateCircle.kt
+++ model/world/src/main/kotlin/de/gleex/pltcmd/model/world/coordinate/CoordinateCircle.kt
@@ -2,12 +2,13 @@ package de.gleex.pltcmd.model.world.coordinate
 
 import de.gleex.pltcmd.util.geometry.circleWithRadius
 import java.util.*
+import java.util.concurrent.ConcurrentSkipListSet
 
 /** A filled circle around a center. */
 class CoordinateCircle(center: Coordinate, radius: Int) : CoordinateArea(center.fillCircle(radius))
 
 fun Coordinate.fillCircle(radius: Int): SortedSet<Coordinate> {
-    val circle = TreeSet<Coordinate>()
+    val circle = ConcurrentSkipListSet<Coordinate>()
     circleWithRadius(eastingFromLeft, northingFromBottom, radius) { x, y ->
         circle.add(Coordinate(x, y))
     }
