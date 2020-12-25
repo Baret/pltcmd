ed65360de7df6a3747bc6a75abc374420a10692a
Baret <450955+Baret@users.noreply.github.com>
2020-12-25 14:36:17 +0100

    Improved structure of SignalVisualizationFragment

--- game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/fragments/tileinformation/SignalVisualizationFragment.kt
+++ game/ui/src/main/kotlin/de/gleex/pltcmd/game/ui/fragments/tileinformation/SignalVisualizationFragment.kt
@@ -4,12 +4,10 @@ import de.gleex.pltcmd.game.engine.Game
 import de.gleex.pltcmd.game.engine.entities.types.*
 import de.gleex.pltcmd.game.ui.renderers.SignalVisualizer
 import de.gleex.pltcmd.model.world.coordinate.Coordinate
-import org.hexworks.cobalt.databinding.api.binding.Binding
 import org.hexworks.cobalt.databinding.api.extension.toProperty
 import org.hexworks.cobalt.databinding.api.property.Property
 import org.hexworks.cobalt.databinding.api.value.ObservableValue
 import org.hexworks.cobalt.databinding.internal.binding.ComputedDualBinding
-import org.hexworks.cobalt.logging.api.LoggerFactory
 import org.hexworks.zircon.api.Fragments
 
 /**
@@ -21,44 +19,42 @@ class SignalVisualizationFragment(
         private val visualizer: SignalVisualizer
 ) : InfoSidebarFragment(currentTile, "Draw signal", 1) {
 
-    companion object {
-        private val log = LoggerFactory.getLogger(SignalVisualizationFragment::class)
-    }
-
-    private val values: List<Pair<String, (SeeingEntity) -> Unit>> = listOf(
+    /**
+     * The values used in the multiselect fragment. It maps the display string to the action that will be
+     * executed when the respective action has been selected.
+     *
+     * In other words the key will simply be used in the frontend. The value is a piece of code calling
+     * the [visualizer].
+     */
+    private val namedActions: Map<String, (ElementEntity) -> Unit> = mapOf(
             "None" to { visualizer.deactivate() },
-            "Vision" to { entity: SeeingEntity ->
-                visualizer.activateWith(entity.visionImmutable)
+            "Vision" to { seeingEntity: SeeingEntity ->
+                visualizer.activateWith(seeingEntity.visionImmutable)
             },
-            "Radio" to {
-                @Suppress("UNCHECKED_CAST")
-                if (it.type is Communicating) {
-                    it as CommunicatingEntity
-                    visualizer.activateWith(it.radioSignal)
-                } else {
-                    log.info("Entity is NOT communicating!")
-                    visualizer.deactivate()
-                }
-            })
+            "Radio" to { communicatingEntity: CommunicatingEntity ->
+                visualizer.activateWith(communicatingEntity.radioSignal)
+            }
+    )
 
-    private val selectedAction: Property<(SeeingEntity) -> Unit> = values.first().second.toProperty()
+    private val selectedAction: Property<(ElementEntity) -> Unit> = namedActions.values.first().toProperty()
 
     private val select = Fragments
-            .multiSelect(SUB_COMPONENT_WIDTH, values)
+            .multiSelect(SUB_COMPONENT_WIDTH, namedActions.toList())
             .withToStringMethod { it.first }
             .withCallback { _, newPair -> selectedAction.updateValue(newPair.second) }
             .build()
 
-    private val visualizerUpdate: Binding<Unit> = ComputedDualBinding(currentInfoTile, selectedAction) { coordinate, drawSignal ->
-        game
-                .firstElementAt(coordinate)
-                .fold(
-                        whenEmpty = { visualizer.deactivate() },
-                        whenPresent = { elementToVisualize -> drawSignal(elementToVisualize) }
-                )
-    }
-
     init {
+        // React to updates of currentInfoTile as well as new selection of the multiselect
+        ComputedDualBinding(currentInfoTile, selectedAction) { coordinate, drawSignal ->
+            game
+                    .firstElementAt(coordinate)
+                    .fold(
+                            whenEmpty = { visualizer.deactivate() },
+                            whenPresent = { elementToVisualize -> drawSignal(elementToVisualize) }
+                    )
+        }
+
         componentsContainer
                 .addFragment(select)
     }
