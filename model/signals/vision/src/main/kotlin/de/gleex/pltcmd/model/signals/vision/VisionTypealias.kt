3a94d03f381f4bc4d073e4e11f16bec91bf010af
Baret <450955+Baret@users.noreply.github.com>
2021-01-15 13:54:19 +0100

    Moved initialVision to its own file.

--- model/signals/vision/src/main/kotlin/de/gleex/pltcmd/model/signals/vision/VisionTypealias.kt
+++ model/signals/vision/src/main/kotlin/de/gleex/pltcmd/model/signals/vision/VisionTypealias.kt
@@ -1,23 +1,8 @@
 package de.gleex.pltcmd.model.signals.vision
 
 import de.gleex.pltcmd.model.signals.core.Signal
-import de.gleex.pltcmd.model.world.WorldArea
-import de.gleex.pltcmd.model.world.WorldTile
-import de.gleex.pltcmd.model.world.coordinate.Coordinate
-import de.gleex.pltcmd.model.world.terrain.Terrain
-import kotlin.random.Random
 
 /**
  * A [Signal] based on [VisionPower].
  */
 typealias Vision = Signal<VisionPower>
-
-/**
- * An "empty vision signal" at an invalid location. Can be used as placeholder when no [WorldArea] can be
- * created.
- */
-fun initialVision(power: VisionPower) = Vision(
-        Coordinate.maximum,
-        WorldArea(sortedSetOf(WorldTile(Coordinate.maximum, Terrain.random(Random)))),
-        power
-)
---
+++ model/signals/vision/src/main/kotlin/de/gleex/pltcmd/model/signals/vision/builder/InitialVision.kt
@@ -0,0 +1,19 @@
+package de.gleex.pltcmd.model.signals.vision.builder
+
+import de.gleex.pltcmd.model.signals.vision.Vision
+import de.gleex.pltcmd.model.signals.vision.VisionPower
+import de.gleex.pltcmd.model.world.WorldArea
+import de.gleex.pltcmd.model.world.WorldTile
+import de.gleex.pltcmd.model.world.coordinate.Coordinate
+import de.gleex.pltcmd.model.world.terrain.Terrain
+import kotlin.random.Random
+
+/**
+ * An "empty vision signal" at an invalid location. Can be used as placeholder when no [WorldArea] can be
+ * created.
+ */
+fun initialVision(power: VisionPower) = Vision(
+    Coordinate.maximum,
+    WorldArea(sortedSetOf(WorldTile(Coordinate.maximum, Terrain.random(Random)))),
+    power
+)
