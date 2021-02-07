22bc11bc34bcaa780115130e1e4bcecb7e3196fb
Loomie <Loomie@users.noreply.github.com>
2021-02-07 10:03:29 +0100

    faction relations

--- model/faction/src/main/kotlin/de/gleex/pltcmd/model/faction/Faction.kt
+++ model/faction/src/main/kotlin/de/gleex/pltcmd/model/faction/Faction.kt
@@ -1,6 +1,10 @@
 package de.gleex.pltcmd.model.faction.de.gleex.pltcmd.model.faction
 
+import de.gleex.pltcmd.model.faction.FactionRelations
+
 /**
  * One party in a military conflict.
  */
-data class Faction(val name: String)
+data class Faction(val name: String) {
+    val relations: FactionRelations = FactionRelations(this)
+}
---
+++ model/faction/src/main/kotlin/de/gleex/pltcmd/model/faction/FactionRelations.kt
@@ -0,0 +1,22 @@
+package de.gleex.pltcmd.model.faction
+
+import de.gleex.pltcmd.model.elements.Affiliation
+import de.gleex.pltcmd.model.faction.de.gleex.pltcmd.model.faction.Faction
+
+/**
+ * Holds the [Affiliation] to other [Faction]s.
+ */
+class FactionRelations(of: Faction) {
+    private val related = mutableMapOf<Faction, Affiliation>()
+
+    init {
+        related.put(of, Affiliation.Self)
+    }
+
+    fun update(other: Faction, state: Affiliation) {
+        related[other] = state
+    }
+
+    fun affiliationOf(other: Faction): Affiliation = related[other] ?: Affiliation.Neutral
+
+}
