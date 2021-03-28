aa2371ea7b4ac389ffc115ab682ce6b59236b005
Loomie <Loomie@users.noreply.github.com>
2021-03-28 12:29:05 +0200

    cleanup

--- game/serialization/src/main/kotlin/Storage.kt
+++ game/serialization/src/main/kotlin/Storage.kt
@@ -1,21 +1,11 @@
 import de.gleex.pltcmd.game.serialization.world.WorldMapDao
-import de.gleex.pltcmd.model.world.Sector
 import de.gleex.pltcmd.model.world.WorldMap
-import de.gleex.pltcmd.model.world.WorldTile
-import de.gleex.pltcmd.model.world.coordinate.Coordinate
-import de.gleex.pltcmd.model.world.coordinate.CoordinateRectangle
-import de.gleex.pltcmd.model.world.terrain.Terrain
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.protobuf.ProtoBuf
 import org.hexworks.cobalt.logging.api.LoggerFactory
 import java.io.File
-import java.nio.charset.Charset
-import kotlin.random.Random
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 import kotlin.time.measureTimedValue
@@ -34,27 +24,22 @@ object Storage {
         }
     }
 
+    /** Save the given map to the specified file. */
     fun save(map: WorldMap, fileId: String) {
         val fileName = toMapFile(fileId)
         log.debug("saving $map to $fileName")
         val duration = measureTime {
             val dao = WorldMapDao.of(map)
-            val json = Json.encodeToString(dao).toByteArray()
-            val protoBuf = ProtoBuf.encodeToByteArray(dao)
-            val cbor = Cbor.encodeToByteArray(dao)
-            // TODO remove debug output
-            println("saved map sizes:")
-            println("JSON ${json.size} " + json.toString(Charset.defaultCharset()).substring(0, 100))
-            println("Proto ${protoBuf.size} " + protoBuf.toString(Charset.defaultCharset()).substring(0, 100))
-            println("CBOR ${cbor.size} " + cbor.toString(Charset.defaultCharset()).substring(0, 100))
+            val bytes = ProtoBuf.encodeToByteArray(dao)
             val file = File(dataFolder, fileName)
-            file.writeBytes(protoBuf)
+            file.writeBytes(bytes)
         }
         log.info("saved $map to $fileName in $duration")
     }
 
     private fun toMapFile(fileId: String) = fileId.fileName + ".map"
 
+    /** Loads the map from the given file. Returns null if the file does not exist. */
     fun loadMap(fileId: String): WorldMap? {
         val fileName = toMapFile(fileId)
         val (map, duration) = measureTimedValue {
@@ -74,19 +59,3 @@ object Storage {
 /** Remove path navigation from name like "../../root" */
 val String.fileName: String
     get() = File(this).nameWithoutExtension
-
-// TODO remove test application
-fun main() {
-    val origin = Coordinate(100, 250)
-    val sectors = (0..4).map { i ->
-        val sectorOrigin = origin.withRelativeEasting(i * Sector.TILE_COUNT)
-        Sector(sectorOrigin, CoordinateRectangle(sectorOrigin, Sector.TILE_COUNT, Sector.TILE_COUNT).asSequence()
-            .map { WorldTile(it, Terrain.random(Random)) }
-            .toSortedSet())
-    }
-    val map = WorldMap.create(sectors)
-    val fileId = "test"
-    Storage.save(map, fileId)
-    val loaded = Storage.loadMap(fileId)
-    println("restored $loaded")
-}
