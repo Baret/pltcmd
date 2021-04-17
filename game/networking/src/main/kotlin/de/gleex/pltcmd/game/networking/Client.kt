71a2acb4d10cd28dacc4e203ca95be438f753071
Loomie <Loomie@users.noreply.github.com>
2021-04-17 13:04:07 +0200

    Main starts server and connects to it

--- game/networking/src/main/kotlin/de/gleex/pltcmd/game/networking/Client.kt
+++ game/networking/src/main/kotlin/de/gleex/pltcmd/game/networking/Client.kt
@@ -10,26 +10,29 @@ import io.ktor.http.cio.websocket.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.protobuf.ProtoBuf
+import org.hexworks.cobalt.logging.api.LoggerFactory
 
 internal const val defaultPort = 9170
+
+private val log = LoggerFactory.getLogger(::connect::class)
 
 fun connect(host: String = "127.0.0.1", port: Int = defaultPort) {
     val client = HttpClient {
         install(WebSockets)
     }
     runBlocking {
-        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = defaultPort, path = pathBroadcastEvents) {
-            repeat(2) {
-                val frame = incoming.receive() as Frame.Binary
+        log.info("Connecting to server $host:$port")
+        client.webSocket(method = HttpMethod.Get, host = host, port = port, path = pathBroadcastEvents) {
+            for (frame in incoming) {
                 val bytes = frame.readBytes()
                 val broadcastEvent = ProtoBuf.decodeFromByteArray<UiBroadcastEvent>(bytes)
-                println(broadcastEvent)
+                log.trace{"received event $broadcastEvent"}
                 uiEventBus.publish(broadcastEvent, UiBroadcasts)
             }
         }
     }
     client.close()
-    println("Connection closed. Goodbye!")
+    log.info("Connection to server closed.")
 }
 
 fun main() {
