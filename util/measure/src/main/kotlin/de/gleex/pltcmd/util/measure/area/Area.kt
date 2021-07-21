de16dc8c30d078db2a047508a12d83d941a10d21
Loomie <Loomie@users.noreply.github.com>
2021-07-21 19:05:33 +0200

    KDoc for Area.times() and Area..div()

--- util/measure/src/main/kotlin/de/gleex/pltcmd/util/measure/area/Area.kt
+++ util/measure/src/main/kotlin/de/gleex/pltcmd/util/measure/area/Area.kt
@@ -7,8 +7,11 @@ data class Area internal constructor(internal val valueInSquareMeters: Double) {
     /** Creates an [Area] of the given size. */
     constructor(value: Double, unit: AreaUnit) : this(unit.inSquareMeters(value))
 
+    /** Return an [Area] that has [multiplier] times the size of this area */
     operator fun times(multiplier: Double) = Area(valueInSquareMeters * multiplier)
+    /** Return the factor between this and the given [Area] */
     operator fun div(other: Area): Double = valueInSquareMeters / other.valueInSquareMeters
 }
 
+/** Return an [Area] that has this often the size of the given area */
 operator fun Double.times(area: Area): Area = area * this
