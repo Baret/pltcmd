62492c5b6dfaa69217ace516ae47bb589bf32a9f
Loomie <Loomie@users.noreply.github.com>
2021-10-17 10:26:06 +0200

    improved comment
    Co-authored-by: Baret <450955+Baret@users.noreply.github.com>

--- model/combat/src/main/kotlin/de/gleex/pltcmd/model/combat/defense/DefenseFactor.kt
+++ model/combat/src/main/kotlin/de/gleex/pltcmd/model/combat/defense/DefenseFactor.kt
@@ -1,7 +1,7 @@
 package de.gleex.pltcmd.model.combat.defense
 
 /**
- * Describes a fact that is considered for defense. So it is able to reduce the chance of being shot by an attack.
+ * Describes a fact that is considered for defense. So it is able to reduce the chance of being hit by an attack.
  **/
 interface DefenseFactor {
     /** A ratio which describes the chance for an attacker to miss a careful shot. Negative values may be used to indicate that other factory are reduced by this factor. */
