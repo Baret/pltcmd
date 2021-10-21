c501697132c7f9cfc15b78bb6344ec2542194863
Baret <450955+Baret@users.noreply.github.com>
2021-10-21 14:16:04 +0200

    No need for an internal setter

--- model/faction/src/main/kotlin/de/gleex/pltcmd/model/faction/graph/AffiliationEdge.kt
+++ model/faction/src/main/kotlin/de/gleex/pltcmd/model/faction/graph/AffiliationEdge.kt
@@ -7,9 +7,9 @@ import org.jgrapht.graph.DefaultEdge
  * An edge representing an [Affiliation]. It is **IMPORTANT** that this is **NOT A DATA CLASS**. Equals and
  * hashCode may not be implemented here!
  */
-internal class AffiliationEdge(affiliation: Affiliation): DefaultEdge() {
-    var affiliation = affiliation
-        internal set
-}
+internal class AffiliationEdge(var affiliation: Affiliation): DefaultEdge()
 
+/**
+ * Creates an [AffiliationEdge] containing this affiliation.
+ */
 internal fun Affiliation.toEdge() = AffiliationEdge(this)
