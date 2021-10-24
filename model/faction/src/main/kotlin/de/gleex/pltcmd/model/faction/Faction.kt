5f5ce1f0ab6c5794077589e1682f12af1de08e8c
Baret <450955+Baret@users.noreply.github.com>
2021-10-24 11:16:31 +0200

    Resolves #158: Factions as graph (#160)
    Welcome to the world of JGraphT :)
    
    The relations between factions are now internally held as a `Graph`. Every newly created Faction registers itself so that the `FactionRelations` object always returns valid values.
    
    The intention of the `graph` module is to serve as THE single module to provide the JGraphT dependency. It will also contain convenience methods to handle graphs more "kotlin style".

--- model/faction/src/main/kotlin/de/gleex/pltcmd/model/faction/Faction.kt
+++ model/faction/src/main/kotlin/de/gleex/pltcmd/model/faction/Faction.kt
@@ -1,6 +1,15 @@
 package de.gleex.pltcmd.model.faction
 
+import mu.KotlinLogging
+
+private val log = KotlinLogging.logger {  }
+
 /**
  * One party in a military conflict.
  */
-data class Faction(val name: String)
+data class Faction(val name: String) {
+    init {
+        log.debug { "Creating faction $name. Registering at FactionRelations." }
+        FactionRelations.register(this)
+    }
+}
