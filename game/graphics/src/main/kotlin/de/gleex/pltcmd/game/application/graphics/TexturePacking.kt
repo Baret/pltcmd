53e1a2b26d6c3b2c6c39f4fbb70eeeaecbd4b3a5
Baret <450955+Baret@users.noreply.github.com>
2023-09-11 22:01:16 +0200

    renamed files and packed into an atlas

--- game/graphics/src/main/kotlin/de/gleex/pltcmd/game/application/graphics/TexturePacking.kt
+++ game/graphics/src/main/kotlin/de/gleex/pltcmd/game/application/graphics/TexturePacking.kt
@@ -2,18 +2,16 @@ package de.gleex.pltcmd.game.application.graphics
 
 import com.badlogic.gdx.tools.texturepacker.TexturePacker
 import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings
-import java.io.File
 
-// TODO: programmatically pack the textures into an atlas
 fun main() {
-    println("processing terrain folder")
-    val baseFolder = ClassLoader.getSystemClassLoader().getResource("terrain/")?.path!!
+    val baseFolder = "game/graphics/src/main/resources/terrain/"
+    println("Packing textures in $baseFolder")
     val settings = Settings().apply {
+        pot = false
         paddingX = 2
         paddingY = 2
-        maxWidth = 104
-        //rotation
+        maxWidth = 120
     }
-    TexturePacker.process(settings, baseFolder, "$baseFolder${File.pathSeparator}packed", "terrain")
+    TexturePacker.process(settings, baseFolder, "${baseFolder}packed", "terrain")
     println("texture packing done")
 }
