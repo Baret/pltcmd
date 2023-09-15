480b46967fe99f7ba93390a2ce8347a311c66957
Baret <450955+Baret@users.noreply.github.com>
2023-09-15 21:04:20 +0200

    puzzled together some basics from the gdx-liftoff
    does not yet start

---
+++ game/application-ktx/src/main/kotlin/de/gleex/pltcmd/game/application/Pltcmd.kt
@@ -0,0 +1,35 @@
+package de.gleex.pltcmd.game.application
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+import ktx.app.clearScreen
+import ktx.assets.disposeSafely
+import ktx.assets.toInternalFile
+import ktx.graphics.use
+
+class Pltcmd : KtxGame<KtxScreen>() {
+    override fun create() {
+        addScreen(FirstScreen())
+        setScreen<FirstScreen>()
+    }
+}
+
+class FirstScreen : KtxScreen {
+    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
+    private val batch = SpriteBatch()
+
+    override fun render(delta: Float) {
+        clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
+        batch.use {
+            it.draw(image, 100f, 160f)
+        }
+    }
+
+    override fun dispose() {
+        image.disposeSafely()
+        batch.disposeSafely()
+    }
+}
