1bae93e27b8e59ce734f876984a4585c167dc79b
Baret <450955+Baret@users.noreply.github.com>
2023-12-26 23:24:10 +0100

    Drawing grid lines

--- game/application-ktx/src/main/kotlin/de/gleex/pltcmd/game/application/actors/WorldMapRendererActor.kt
+++ game/application-ktx/src/main/kotlin/de/gleex/pltcmd/game/application/actors/WorldMapRendererActor.kt
@@ -4,12 +4,14 @@ import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
 import de.gleex.pltcmd.model.world.WorldMap
 import de.gleex.pltcmd.model.world.WorldTile
 import de.gleex.pltcmd.model.world.coordinate.Coordinate
+import de.gleex.pltcmd.model.world.sectorOrigin
 import de.gleex.pltcmd.model.world.terrain.TerrainType
 import mu.KotlinLogging
 
@@ -59,29 +61,41 @@ class WorldMapRendererActor(private val worldMap: WorldMap) : Actor() {
 
         renderer.fillBackground()
 
-        var currentCoordinate = bottomLeftCoordinate
-        var currentDrawPosition: Pair<Float, Float>
-        do {
-            if (currentCoordinate in worldMap) {
-                renderer.drawTile(worldMap[currentCoordinate])
-            }
-            currentCoordinate = currentCoordinate.movedBy(1, 0)
-            currentDrawPosition = drawPositionOf(currentCoordinate)
-            if (currentDrawPosition.first >= width) {
-                currentCoordinate = currentCoordinate.withEasting(bottomLeftCoordinate.eastingFromLeft).movedBy(0, 1)
-                currentDrawPosition = drawPositionOf(currentCoordinate)
-            }
-        } while (currentDrawPosition.first < width && currentDrawPosition.second < height)
+        renderer.drawTiles()
+
+        renderer.drawGridLines()
 
         batch?.begin()
     }
 
+    private fun ShapeRenderer.drawTiles() {
+        forEachVisibleCoordinate { currentCoordinate ->
+            if (currentCoordinate in worldMap) {
+                drawTile(worldMap[currentCoordinate])
+            }
+        }
+    }
+
+    private fun forEachVisibleCoordinate(action: (Coordinate) -> Unit) {
+        var currentCoordinate = bottomLeftCoordinate
+        var currentDrawPosition: Vector2
+        do {
+            action(currentCoordinate)
+            currentCoordinate = currentCoordinate.movedBy(1, 0)
+            currentDrawPosition = drawPositionOf(currentCoordinate)
+            if (currentDrawPosition.x >= width) {
+                currentCoordinate = currentCoordinate.withEasting(bottomLeftCoordinate.eastingFromLeft).movedBy(0, 1)
+                currentDrawPosition = drawPositionOf(currentCoordinate)
+            }
+        } while (currentDrawPosition.x < width && currentDrawPosition.y < height)
+    }
+
     private fun ShapeRenderer.drawTile(worldTile: WorldTile) {
-        val (drawPosX, drawPosY) = drawPositionOf(worldTile.coordinate)
+        val drawPos = drawPositionOf(worldTile.coordinate)
         drawWithType(ShapeRenderer.ShapeType.Filled) {
             // TODO: draw height
             color = terrainTypeColor[worldTile.type]
-            rect(drawPosX, drawPosY, TILE_WIDTH, TILE_HEIGHT)
+            rect(drawPos.x, drawPos.y, TILE_WIDTH, TILE_HEIGHT)
         }
     }
 
@@ -89,6 +103,28 @@ class WorldMapRendererActor(private val worldMap: WorldMap) : Actor() {
         drawWithType(ShapeRenderer.ShapeType.Filled) {
             color = this@WorldMapRendererActor.color
             rect(0f, 0f, width, height)
+        }
+    }
+
+    private fun ShapeRenderer.drawGridLines() {
+        forEachVisibleCoordinate { currentCoordinate ->
+            val drawPos = drawPositionOf(currentCoordinate)
+            drawWithType(ShapeRenderer.ShapeType.Line) {
+                color = if(currentCoordinate.northingFromBottom == currentCoordinate.sectorOrigin.northingFromBottom) {
+                    Color.RED
+                } else {
+                    Color.BLACK
+                }
+                val bottomRight = Vector2(drawPos).add(TILE_WIDTH, 0f)
+                line(drawPos, bottomRight)
+                color = if(currentCoordinate.eastingFromLeft == currentCoordinate.sectorOrigin.eastingFromLeft) {
+                    Color.RED
+                } else {
+                    Color.BLACK
+                }
+                val topLeft = Vector2(drawPos).add(0f, TILE_HEIGHT)
+                line(drawPos, topLeft)
+            }
         }
     }
 
@@ -104,11 +140,11 @@ class WorldMapRendererActor(private val worldMap: WorldMap) : Actor() {
     /**
      * Translates the given coordinate to the position inside of this actor.
      */
-    private fun drawPositionOf(coordinate: Coordinate): Pair<Float, Float> {
+    private fun drawPositionOf(coordinate: Coordinate): Vector2 {
         val currentOriginCoordinate = coordinate - bottomLeftCoordinate
         val drawPosX: Float = currentOriginCoordinate.eastingFromLeft.toFloat() * TILE_WIDTH
         val drawPosY: Float = currentOriginCoordinate.northingFromBottom.toFloat() * TILE_HEIGHT
-        return drawPosX to drawPosY
+        return Vector2(drawPosX, drawPosY)
     }
 
     private fun scrollByCoordinates(scrollAmountEasting: Int, scrollAmountNorthing: Int) {
@@ -121,7 +157,7 @@ class WorldMapRendererActor(private val worldMap: WorldMap) : Actor() {
     companion object {
         private val terrainTypeColor: Map<TerrainType, Color> = TerrainType.entries.toTypedArray().associate {
             when (it) {
-                TerrainType.GRASSLAND     -> it to Color.GREEN
+                TerrainType.GRASSLAND     -> it to Color.LIGHT_GRAY
                 TerrainType.FOREST        -> it to Color.OLIVE
                 TerrainType.HILL          -> it to Color.GRAY
                 TerrainType.MOUNTAIN      -> it to Color.BROWN
