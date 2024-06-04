000b85b3868b218701ac81c79c1adccd45fbda0d
Baret <450955+Baret@users.noreply.github.com>
2024-06-04 15:22:54 +0200

    fixed typo in parameter

--- util/namegeneration/src/main/kotlin/de/gleex/pltcmd/util/namegeneration/wordlists/WordListExtensions.kt
+++ util/namegeneration/src/main/kotlin/de/gleex/pltcmd/util/namegeneration/wordlists/WordListExtensions.kt
@@ -3,12 +3,12 @@ package de.gleex.pltcmd.util.namegeneration.wordlists
 import de.gleex.kng.api.WordList
 import de.gleex.kng.wordlist.asWordList
 
-internal operator fun WordList.plus(otherWorlist: WordList): WordList =
+internal operator fun WordList.plus(otherWordlist: WordList): WordList =
     buildList {
         for(i in 0 until size) {
             add(get(i))
         }
-        for(j in 0 until  otherWorlist.size) {
-            add(otherWorlist[j])
+        for(j in 0 until  otherWordlist.size) {
+            add(otherWordlist[j])
         }
     }.asWordList()
